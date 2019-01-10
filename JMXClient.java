dc8d579251081341b0bcb4233908d9e014f5322a
Andrew Azores <me@andrewazor.es>
2019-01-10 15:10:52 -0500

    Add input loop

--- JMXClient.java
+++ JMXClient.java
@@ -3,10 +3,26 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 class JMXClient {
     public static void main(String[] args) throws Exception {
         JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:9091/jmxrmi");
         JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
         MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+
+        Scanner scanner = new Scanner(System.in);
+        String in;
+        do {
+            System.out.print("> ");
+            try {
+                in = scanner.nextLine().trim();
+            } catch (NoSuchElementException e) {
+                break;
+            }
+            System.out.println(String.format("\t%s", in));
+        } while (!in.toLowerCase().equals("exit") && !in.toLowerCase().equals("quit"));
+        System.out.println("Exit");
     }
 }
