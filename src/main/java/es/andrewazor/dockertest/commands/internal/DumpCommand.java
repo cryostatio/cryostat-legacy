c1cd552397cbdc4c34b8189b271460fcabd7e692
Andrew Azores <me@andrewazor.es>
2019-01-15 17:16:15 -0500

    Allow recording name to be specified

--- src/main/java/es/andrewazor/dockertest/commands/internal/DumpCommand.java
+++ src/main/java/es/andrewazor/dockertest/commands/internal/DumpCommand.java
@@ -1,5 +1,8 @@
 package es.andrewazor.dockertest.commands.internal;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.openjdk.jmc.common.unit.IConstrainedMap;
 import org.openjdk.jmc.flightrecorder.configuration.recording.RecordingOptionsBuilder;
 import org.openjdk.jmc.rjmx.services.jfr.FlightRecorderException;
@@ -16,7 +19,14 @@ class DumpCommand implements Command {
 
     @Override
     public void execute(IFlightRecorderService service, String[] args) throws Exception {
+        String name;
+        if (args.length == 0) {
+            name = LocalDate.now() + "-" + LocalTime.now();
+        } else {
+            name = args[0];
+        }
         IConstrainedMap<String> recordingOptions = new RecordingOptionsBuilder(service)
+            .name(name)
             .duration(10000)
             .build();
         service.start(recordingOptions, null);
