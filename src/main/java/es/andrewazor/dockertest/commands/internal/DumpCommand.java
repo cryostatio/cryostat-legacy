d28f48e43e5157c714d7f0487e0b14458a6a77ac
Andrew Azores <me@andrewazor.es>
2019-01-16 15:20:17 -0500

    Validate names are unique when dumping

--- src/main/java/es/andrewazor/dockertest/commands/internal/DumpCommand.java
+++ src/main/java/es/andrewazor/dockertest/commands/internal/DumpCommand.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import org.openjdk.jmc.common.unit.IConstrainedMap;
 import org.openjdk.jmc.flightrecorder.configuration.recording.RecordingOptionsBuilder;
 import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
+import org.openjdk.jmc.rjmx.services.jfr.IRecordingDescriptor;
 
 class DumpCommand extends AbstractCommand {
     DumpCommand(IFlightRecorderService service) {
@@ -24,6 +25,14 @@ class DumpCommand extends AbstractCommand {
     public void execute(String[] args) throws Exception {
         String name = args[0];
         int seconds = Integer.parseInt(args[1]);
+
+        for (IRecordingDescriptor recording : service.getAvailableRecordings()) {
+            if (recording.getName().equals(name)) {
+                System.out.println(String.format("Recording with name %s already exists", name));
+                return;
+            }
+        }
+
         IConstrainedMap<String> recordingOptions = new RecordingOptionsBuilder(service)
             .name(name)
             .duration(1000 * seconds)
