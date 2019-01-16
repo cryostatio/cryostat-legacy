38853ca4a0664f4f4bc6087accab61576a15549a
Andrew Azores <me@andrewazor.es>
2019-01-16 10:10:51 -0500

    Formatting

--- src/main/java/es/andrewazor/dockertest/commands/CommandRegistryFactory.java
+++ src/main/java/es/andrewazor/dockertest/commands/CommandRegistryFactory.java
@@ -5,7 +5,7 @@ import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
 import es.andrewazor.dockertest.commands.internal.CommandRegistryImpl;
 
 public class CommandRegistryFactory {
-    private CommandRegistryFactory() {  }
+    private CommandRegistryFactory() { }
 
     public static CommandRegistry createNewInstance(IFlightRecorderService service) throws Exception {
         return new CommandRegistryImpl(service);
