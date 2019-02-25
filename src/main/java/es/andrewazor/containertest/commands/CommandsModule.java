fac9eee9766cec351ccc5b475222e32b499d8ee6
Andrew Azores <me@andrewazor.es>
2019-02-25 10:41:08 -0500

    Remove redundant annotation

--- src/main/java/es/andrewazor/containertest/commands/CommandsModule.java
+++ src/main/java/es/andrewazor/containertest/commands/CommandsModule.java
@@ -2,8 +2,6 @@ package es.andrewazor.containertest.commands;
 
 import java.util.Set;
 
-import javax.inject.Singleton;
-
 import dagger.Module;
 import dagger.Provides;
 import es.andrewazor.containertest.commands.internal.CommandsInternalModule;
@@ -11,7 +9,7 @@ import es.andrewazor.containertest.commands.internal.ConnectionListenerModule;
 
 @Module(includes = { CommandsInternalModule.class, ConnectionListenerModule.class })
 public class CommandsModule {
-    @Provides @Singleton CommandRegistry provideCommandRegistry(Set<Command> commands) {
+    @Provides CommandRegistry provideCommandRegistry(Set<Command> commands) {
         return new CommandRegistry(commands);
     }
 }
