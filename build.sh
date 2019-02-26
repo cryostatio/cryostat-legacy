96753f2f4efa257ababbaa08b5e42953f3955723
Andrew Azores <me@andrewazor.es>
2019-02-26 15:11:18 -0500

    Remove demos and Listener from repository

--- build.sh
+++ build.sh
@@ -8,6 +8,5 @@ if [ -z "$CMD" ]; then
 fi
 
 ./gradlew clean build
-$CMD build -f Dockerfile.docker.listener -t andrewazores/container-jmx-docker-listener:latest .
-$CMD build -f Dockerfile.podman.listener -t andrewazores/container-jmx-podman-listener:latest .
 $CMD build -f Dockerfile.client -t andrewazores/container-jmx-client:latest .
+$CMD push andrewazores/container-jmx-client
