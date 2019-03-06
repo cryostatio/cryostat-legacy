632f53e17cc2bdea96cde66d5208a82bf0f48db0
Andrew Azores <me@andrewazor.es>
2019-03-06 15:03:38 -0500

    search-events filters on more fields of events than key

--- src/main/java/es/andrewazor/containertest/commands/internal/SearchEventsCommand.java
+++ src/main/java/es/andrewazor/containertest/commands/internal/SearchEventsCommand.java
@@ -1,6 +1,9 @@
 package es.andrewazor.containertest.commands.internal;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
@@ -28,7 +31,7 @@ class SearchEventsCommand extends AbstractConnectedCommand {
     public void execute(String[] args) throws Exception {
         Collection<? extends IEventTypeInfo> matchingEvents = getService().getAvailableEventTypes()
             .stream()
-            .filter(event -> event.getEventTypeID().getFullKey().toLowerCase().contains(args[0].toLowerCase()))
+            .filter(event -> eventMatchesSearchTerm(event, args[0].toLowerCase()))
             .collect(Collectors.toList());
 
         if (matchingEvents.isEmpty()) {
@@ -39,5 +42,19 @@ class SearchEventsCommand extends AbstractConnectedCommand {
 
     private void printEvent(IEventTypeInfo event) {
         System.out.println(String.format("\t%s\toptions: %s", event.getEventTypeID().getFullKey(), event.getOptionDescriptors().keySet().toString()));
+    }
+
+    private boolean eventMatchesSearchTerm(IEventTypeInfo event, String term) {
+        Set<String> terms = new HashSet<>();
+        terms.add(event.getEventTypeID().getFullKey());
+        terms.addAll(Arrays.asList(event.getHierarchicalCategory()));
+        terms.add(event.getDescription());
+        terms.add(event.getName());
+
+        return terms
+            .stream()
+            .filter(s -> s != null)
+            .map(String::toLowerCase)
+            .anyMatch(s -> s.contains(term));
     }
 }
