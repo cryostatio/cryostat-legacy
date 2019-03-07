46551a0bc1f038be8e293d5e1dcf1bcb1e029b7a
Andrew Azores <me@andrewazor.es>
2019-03-07 11:07:59 -0500

    Allow client introspection

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+
+function cleanup() {
+    set +e
+    # TODO: better container management
+    docker kill $(docker ps -a -q --filter ancestor=andrewazores/container-jmx-client)
+    docker rm $(docker ps -a -q --filter ancestor=andrewazores/container-jmx-client)
+}
+
+cleanup
+trap cleanup EXIT
+
+set +e
+docker network create --attachable jmx-test
+set -e
+
+docker run \
+    --net=jmx-test \
+    --name jmx-client \
+    -e CONTAINER_DOWNLOAD_HOST=$CONTAINER_DOWNLOAD_HOST \
+    -e CONTAINER_DOWNLOAD_PORT=$CONTAINER_DOWNLOAD_PORT \
+    --rm -it andrewazores/container-jmx-client "$@"
