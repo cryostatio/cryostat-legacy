c06b104396e6c65d0dc763c40795336037c91468
Andrew Azores <me@andrewazor.es>
2019-03-21 00:14:02 -0400

    tmp initial test

---
+++ src/test/java/es/andrewazor/containertest/commands/CommandRegistryTest.java
@@ -0,0 +1,157 @@
+package es.andrewazor.containertest.commands;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import es.andrewazor.containertest.commands.CommandRegistry.CommandDefinitionException;
+
+public class CommandRegistryTest {
+
+    static class WithEmptySetCommands {
+        private CommandRegistry registry;
+
+        @BeforeEach
+        public void setup() {
+            registry = new CommandRegistry(Collections.emptySet());
+        }
+
+        @Test
+        public void shouldReturnEmptyRegisteredCommandNames() {
+            assertThat("registered commands should be empty", registry.getRegisteredCommandNames(),
+                    equalTo(Collections.emptySet()));
+        }
+
+        @Test
+        public void shouldReturnEmptyAvailableCommandNames() {
+            assertThat("available commands should be empty", registry.getAvailableCommandNames(),
+                    equalTo(Collections.emptySet()));
+        }
+
+        @Test
+        public void shouldNoOpOnExecute() throws Exception {
+            registry.execute("foo", new String[] {});
+        }
+    }
+
+    static class WithCommandDefinitions {
+        private CommandRegistry registry;
+
+        private FooCommand fooCommand = new FooCommand();
+        private BarCommand barCommand = new BarCommand();
+
+        private Command[] commands = new Command[] { fooCommand, barCommand };
+
+        @BeforeEach
+        public void setup() {
+            registry = new CommandRegistry(new HashSet<Command>(Arrays.asList(commands)));
+        }
+
+        @Test
+        public void shouldReturnRegisteredCommandNames() {
+            assertThat("registered command names should be returned", registry.getRegisteredCommandNames(),
+                    equalTo(new HashSet<String>(Arrays.asList(fooCommand.getName(), barCommand.getName()))));
+        }
+
+        @Test
+        public void shouldReturnAvailableCommandNames() {
+            assertThat("available command names should be returned", registry.getAvailableCommandNames(),
+                    equalTo(new HashSet<String>(Arrays.asList(fooCommand.getName()))));
+        }
+
+        @Test
+        public void shouldExecuteRegisteredAndAvailableCommand() throws Exception {
+            assertThat("command should not have been executed", fooCommand.value, nullValue());
+            registry.execute("foo", new String[] { "arg" });
+            assertThat("command should have been executed", fooCommand.value, equalTo("arg"));
+        }
+
+        @Test
+        public void shouldNotExecuteRegisteredAndUnavailableCommand() throws Exception {
+            assertThat("command should not have been executed", barCommand.value, nullValue());
+            registry.execute("bar", new String[] { "arg" });
+            assertThat("command should not have been executed", barCommand.value, nullValue());
+        }
+
+        @Test
+        public void shouldNoOpOnUnregisteredCommand() throws Exception {
+            assertThat("command should not have been executed", fooCommand.value, nullValue());
+            assertThat("command should not have been executed", barCommand.value, nullValue());
+            registry.execute("baz", new String[] { "arg" });
+            assertThat("command should not have been executed", fooCommand.value, nullValue());
+            assertThat("command should not have been executed", barCommand.value, nullValue());
+        }
+    }
+
+    static class WithConflictingCommandDefinitions {
+        @Test
+        public void shouldThrowCommandDefinitionException() {
+            CommandDefinitionException thrown = assertThrows(CommandDefinitionException.class,
+                    () -> new CommandRegistry(
+                            new HashSet<Command>(Arrays.asList(new FooCommand(), new DuplicateFooCommand()))),
+                    "should throw CommandDefinitionException for duplicate definitions");
+            assertThat(thrown.getMessage(), equalTo(
+                    "\"foo\" command definitions provided by class es.andrewazor.containertest.commands.CommandRegistryTest.DuplicateFooCommand AND class es.andrewazor.containertest.commands.CommandRegistryTest.FooCommand"));
+        }
+    }
+
+    static class FooCommand implements Command {
+        String value = null;
+
+        @Override
+        public String getName() {
+            return "foo";
+        }
+
+        @Override
+        public boolean isAvailable() {
+            return true;
+        }
+
+        @Override
+        public boolean validate(String[] args) {
+            return true;
+        }
+
+        @Override
+        public void execute(String[] args) {
+            this.value = args[0];
+        }
+    }
+
+    static class BarCommand implements Command {
+        String value = null;
+
+        @Override
+        public String getName() {
+            return "bar";
+        }
+
+        @Override
+        public boolean isAvailable() {
+            return false;
+        }
+
+        @Override
+        public boolean validate(String[] args) {
+            return false;
+        }
+
+        @Override
+        public void execute(String[] args) {
+            this.value = args[0];
+        }
+    }
+
+    static class DuplicateFooCommand extends FooCommand {
+    }
+
+}
