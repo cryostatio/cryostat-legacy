8ac29e265e2ef0eb8f531bff89f5c645475b9a3b
Andrew Azores <me@andrewazor.es>
2019-05-09 12:53:49 -0400

    Implement PortScanCommand

--- src/main/java/es/andrewazor/containertest/commands/internal/CommandsInternalModule.java
+++ src/main/java/es/andrewazor/containertest/commands/internal/CommandsInternalModule.java
@@ -32,6 +32,7 @@ public abstract class CommandsInternalModule {
     @Binds @IntoSet abstract Command bindListEventTypesCommand(ListEventTypesCommand command);
     @Binds @IntoSet abstract Command bindListRecordingOptionsCommand(ListRecordingOptionsCommand command);
     @Binds @IntoSet abstract Command bindPingCommand(PingCommand command);
+    @Binds @IntoSet abstract Command bindPortScanCommand(PortScanCommand command);
     @Binds @IntoSet abstract Command bindPrintUrlCommand(PrintUrlCommand command);
     @Binds @IntoSet abstract Command bindRecordingOptionsCustomizerCommand(RecordingOptionsCustomizerCommand command);
     @Binds @IntoSet abstract Command bindSearchEventsCommand(SearchEventsCommand command);
