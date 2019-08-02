d75edf5eda73d2321638957c80cb89d6b4b68cce
Andrew Azores <me@andrewazor.es>
2019-08-02 09:21:21 -0400

    Search all service ports, not just known default

--- src/main/java/com/redhat/rhjmc/containerjfr/platform/KubePlatformClient.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/platform/KubePlatformClient.java
@@ -3,9 +3,9 @@ package com.redhat.rhjmc.containerjfr.platform;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.redhat.rhjmc.containerjfr.core.util.log.Logger;
 
@@ -14,12 +14,13 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1Service;
+import io.kubernetes.client.models.V1ServiceList;
+import io.kubernetes.client.models.V1ServicePort;
+import io.kubernetes.client.models.V1ServiceSpec;
 import io.kubernetes.client.util.Config;
 
 class KubePlatformClient implements PlatformClient {
 
-    // TODO implement search method that is service port agnostic
-    private static final int EXPECTED_SERVICE_PORT = 9091;
     private final Logger logger;
     private final CoreV1Api api;
 
@@ -31,18 +32,17 @@ class KubePlatformClient implements PlatformClient {
     @Override
     public List<ServiceRef> listDiscoverableServices() {
         try {
-            return this.api
-                .listNamespacedService(Files.readString(Paths.get(Config.SERVICEACCOUNT_ROOT, "namespace")).trim(), null, null, null, null, null, null, null, null, null)
-                .getItems()
-                .stream()
-                .map(V1Service::getSpec)
-                .filter(s -> s.getPorts().stream().anyMatch(p -> p.getPort() == EXPECTED_SERVICE_PORT))
-                .map(s -> new ServiceRef(
-                                s.getExternalName(),
-                                s.getClusterIP(),
-                                EXPECTED_SERVICE_PORT
-                            ))
-                .collect(Collectors.toList());
+            String currentNamespace = Files.readString(Paths.get(Config.SERVICEACCOUNT_ROOT, "namespace")).trim();
+            V1ServiceList services = api
+                .listNamespacedService(currentNamespace, null, null, null, null, null, null, null, null, null);
+            List<ServiceRef> result = new ArrayList<>();
+            for (V1Service service : services.getItems()) {
+                V1ServiceSpec spec = service.getSpec();
+                for (V1ServicePort port : spec.getPorts()) {
+                    result.add(new ServiceRef(spec.getExternalName(), spec.getClusterIP(), port.getPort()));
+                }
+            }
+            return Collections.unmodifiableList(result);
         } catch (IOException e) {
             logger.warn(e.getMessage());
             logger.warn(ExceptionUtils.getStackTrace(e));
