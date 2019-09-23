a5c120e0c43ba147bf3ffcedff30d97124339c42
tabjy <8033899+tabjy@users.noreply.github.com>
2019-09-23 23:47:06 +0800

    Improve tests for SocketClientReaderWriter and NetworkResolver (#33)
    * Improve tests for SocketClientReaderWriter and NetworkResolver
    
    * Refactor socket variable names
    
    * Remove releasing semaphore in the testing-only constructor

--- src/main/java/com/redhat/rhjmc/containerjfr/tui/tcp/SocketClientReaderWriter.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/tui/tcp/SocketClientReaderWriter.java
@@ -5,6 +5,7 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
@@ -16,20 +17,21 @@ class SocketClientReaderWriter implements ClientReader, ClientWriter {
 
     private final Logger logger;
     private final Thread listenerThread;
-    private final ServerSocket ss;
-    private final Semaphore semaphore = new Semaphore(0, true);
-    private volatile Socket s;
+    private final ServerSocket serverSocket;
+    private final Semaphore semaphore;
+    private volatile Socket socket;
     private volatile Scanner scanner;
     private volatile OutputStreamWriter writer;
 
     SocketClientReaderWriter(Logger logger, int port) throws IOException {
         this.logger = logger;
-        ss = new ServerSocket(port);
+        serverSocket = new ServerSocket(port);
+        semaphore = new Semaphore(0, true);
         listenerThread = new Thread(() -> {
             System.out.println(String.format("Listening on port %d", port));
             while (true) {
                 try {
-                    Socket sock = ss.accept();
+                    Socket sock = serverSocket.accept();
                     try {
                         close();
                     } catch (IOException e) {
@@ -37,9 +39,9 @@ class SocketClientReaderWriter implements ClientReader, ClientWriter {
                     }
                     System.out.println(String.format("Connected: %s", sock.getRemoteSocketAddress().toString()));
                     try {
-                        s = sock;
-                        scanner = new Scanner(sock.getInputStream(), "utf-8");
-                        writer = new OutputStreamWriter(sock.getOutputStream(), "utf-8");
+                        socket = sock;
+                        scanner = new Scanner(sock.getInputStream(), StandardCharsets.UTF_8);
+                        writer = new OutputStreamWriter(sock.getOutputStream(), StandardCharsets.UTF_8);
                     } finally {
                         semaphore.release();
                     }
@@ -53,6 +55,17 @@ class SocketClientReaderWriter implements ClientReader, ClientWriter {
         listenerThread.start();
     }
 
+    // Testing-only constructor
+    SocketClientReaderWriter(Logger logger, Semaphore semaphore, Socket socket, Scanner scanner, OutputStreamWriter writer) {
+        this.logger = logger;
+        this.semaphore = semaphore;
+        this.scanner = scanner;
+        this.writer = writer;
+        this.listenerThread = null;
+        this.serverSocket = null;
+        this.socket = socket;
+    }
+
     @Override
     public void close() throws IOException {
         semaphore.drainPermits();
@@ -62,8 +75,8 @@ class SocketClientReaderWriter implements ClientReader, ClientWriter {
         if (writer != null) {
             writer.close();
         }
-        if (s != null) {
-            s.close();
+        if (socket != null) {
+            socket.close();
         }
     }
 
