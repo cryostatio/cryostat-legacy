b73a5165052b9f6836a35af2fd4fb1c64add0c6c
Andrew Azores <me@andrewazor.es>
2020-01-21 16:36:23 -0500

    Use env var to configure authmanager selection
    Related to #102

--- src/main/java/com/redhat/rhjmc/containerjfr/net/AuthManager.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/net/AuthManager.java
@@ -3,6 +3,12 @@ package com.redhat.rhjmc.containerjfr.net;
 import java.util.concurrent.Future;
 import java.util.function.Supplier;
 
+/**
+ * AuthManager implementations must also include a constructor which takes a single parameter of a
+ * {@link com.redhat.rhjmc.containerjfr.core.log.Logger}. This is to enable runtime configurable
+ * selection of the activated AuthManager by reflection. This interface is meant as internal API
+ * and so this requirement is subject to change.
+ */
 public interface AuthManager {
     Future<Boolean> validateToken(Supplier<String> tokenProvider) throws Exception;
 
