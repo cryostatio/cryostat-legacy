6bfd88284a5a68b54652e14e25ce5cbf9d9d13dd
Andrew Azores <me@andrewazor.es>
2020-02-06 21:35:50 +0000

    Configurable platform (#126)
    * Fail fast if configured auth manager is not found
    
    Do not fall back to platform detection strategy if the user specifies an
    auth manager implementation which is not found. Instead, throw an
    exception and exit
    
    Related to #124
    
    * Use env var to configure platform strategy selection
    
    Fixes #124

--- src/main/java/com/redhat/rhjmc/containerjfr/platform/PlatformModule.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/platform/PlatformModule.java
@@ -1,11 +1,13 @@
 package com.redhat.rhjmc.containerjfr.platform;
 
+import java.util.Objects;
 import java.util.Set;
 
 import javax.inject.Singleton;
 
 import com.redhat.rhjmc.containerjfr.core.log.Logger;
 import com.redhat.rhjmc.containerjfr.core.net.discovery.JvmDiscoveryClient;
+import com.redhat.rhjmc.containerjfr.core.sys.Environment;
 import com.redhat.rhjmc.containerjfr.platform.internal.PlatformDetectionStrategy;
 import com.redhat.rhjmc.containerjfr.platform.internal.PlatformStrategyModule;
 
@@ -14,9 +16,26 @@ import dagger.Provides;
 
 @Module(includes = {PlatformStrategyModule.class})
 public abstract class PlatformModule {
+
+    static final String PLATFORM_STRATEGY_ENV_VAR = "CONTAINER_JFR_PLATFORM";
+
     @Provides
     @Singleton
-    static PlatformClient providePlatformClient(Set<PlatformDetectionStrategy<?>> strategies) {
+    static PlatformClient providePlatformClient(
+            Set<PlatformDetectionStrategy<?>> strategies, Environment env, Logger logger) {
+        if (env.hasEnv(PLATFORM_STRATEGY_ENV_VAR)) {
+            String platform = env.getEnv(PLATFORM_STRATEGY_ENV_VAR);
+            logger.info(
+                    String.format(
+                            "Selecting configured PlatformDetectionStrategy \"%s\"", platform));
+            for (PlatformDetectionStrategy<?> strat : strategies) {
+                if (Objects.equals(platform, strat.getClass().getCanonicalName())) {
+                    return new SelfDiscoveryPlatformClient(strat.get());
+                }
+            }
+            throw new RuntimeException(
+                    String.format("Selected PlatformDetectionStrategy \"%s\" not found", platform));
+        }
         return new SelfDiscoveryPlatformClient(
                 strategies.stream()
                         .sorted()
