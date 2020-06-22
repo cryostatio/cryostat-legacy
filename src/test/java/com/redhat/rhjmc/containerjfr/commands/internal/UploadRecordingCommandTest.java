4e93167cb36a5c52615fca8c175d78259eccde11
Andrew Azores <me@andrewazor.es>
2020-06-22 16:04:42 -0400

    Correct tests

--- src/test/java/com/redhat/rhjmc/containerjfr/commands/internal/UploadRecordingCommandTest.java
+++ src/test/java/com/redhat/rhjmc/containerjfr/commands/internal/UploadRecordingCommandTest.java
@@ -41,23 +41,18 @@
  */
 package com.redhat.rhjmc.containerjfr.commands.internal;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.commons.lang3.tuple.Pair;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -66,7 +61,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
 import org.openjdk.jmc.rjmx.services.jfr.IRecordingDescriptor;
@@ -82,8 +79,14 @@ import com.redhat.rhjmc.containerjfr.core.tui.ClientWriter;
 import com.redhat.rhjmc.containerjfr.net.TargetConnectionManager;
 import com.redhat.rhjmc.containerjfr.net.TargetConnectionManager.ConnectedTask;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+
 @ExtendWith(MockitoExtension.class)
-@Disabled("TODO")
 class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordingName {
 
     static final String HOST_ID = "fooHost:9091";
@@ -94,7 +97,7 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
     @Mock TargetConnectionManager targetConnectionManager;
     @Mock FileSystem fs;
     @Mock Path path;
-    @Mock CloseableHttpClient httpClient;
+    @Mock WebClient webClient;
     @Mock JFRConnection conn;
 
     @Override
@@ -109,9 +112,8 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
 
     @BeforeEach
     void setup() {
-        // this.command =
-        //         new UploadRecordingCommand(cw, targetConnectionManager, fs, path, () ->
-        // httpClient);
+        this.command =
+                new UploadRecordingCommand(cw, targetConnectionManager, fs, path, () -> webClient);
     }
 
     @Test
@@ -164,18 +166,20 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(conn.getService()).thenReturn(svc);
             Mockito.when(svc.getAvailableRecordings()).thenReturn(List.of(rec));
             Mockito.when(rec.getName()).thenReturn("foo");
-            Mockito.when(command.targetConnectionManager.connect(Mockito.anyString()))
-                    .thenReturn(conn);
             Mockito.when(svc.openStream(Mockito.any(), Mockito.anyBoolean())).thenReturn(stream);
 
-            // FIXME
-            // UploadRecordingCommand.RecordingConnection res =
-            //         command.getBestRecordingForName(HOST_ID, rec.getName());
+            Optional<Pair<Path, Boolean>> res =
+                    command.getBestRecordingForName(HOST_ID, rec.getName());
 
-            // Assertions.assertTrue(res.getStream().isPresent());
-            // Assertions.assertTrue(res.getConnection().isPresent());
-            // MatcherAssert.assertThat(res.getStream().get(), Matchers.sameInstance(stream));
-            // Mockito.verify(svc).openStream(rec, false);
+            Assertions.assertTrue(res.isPresent());
+            Assertions.assertNotNull(res.get().getLeft());
+            Assertions.assertTrue(res.get().getRight());
+            Mockito.verify(fs)
+                    .copy(
+                            Mockito.same(stream),
+                            Mockito.any(Path.class),
+                            Mockito.eq(StandardCopyOption.REPLACE_EXISTING));
+            Mockito.verify(svc).openStream(rec, false);
         }
 
         @Test
@@ -194,16 +198,13 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(path.resolve(Mockito.anyString())).thenReturn(rec);
             Mockito.when(fs.isRegularFile(rec)).thenReturn(true);
             Mockito.when(fs.isReadable(rec)).thenReturn(true);
-            Mockito.when(fs.newInputStream(rec)).thenReturn(stream);
 
-            // FIXME
-            // UploadRecordingCommand.RecordingConnection res =
-            //         command.getBestRecordingForName(HOST_ID, "foo");
+            Optional<Pair<Path, Boolean>> res = command.getBestRecordingForName(HOST_ID, "foo");
 
-            // Assertions.assertTrue(res.getStream().isPresent());
-            // Assertions.assertFalse(res.getConnection().isPresent());
-            // MatcherAssert.assertThat(
-            //         res.getStream().get(), Matchers.instanceOf(BufferedInputStream.class));
+            Assertions.assertTrue(res.isPresent());
+            Assertions.assertNotNull(res.get().getLeft());
+            Assertions.assertFalse(res.get().getRight());
+            MatcherAssert.assertThat(res.get().getLeft(), Matchers.sameInstance(rec));
         }
 
         @Test
@@ -221,12 +222,9 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(path.resolve(Mockito.anyString())).thenReturn(rec);
             Mockito.when(fs.isRegularFile(rec)).thenReturn(false);
 
-            // FIXME
-            // UploadRecordingCommand.RecordingConnection res =
-            //         command.getBestRecordingForName(HOST_ID, "foo");
+            Optional<Pair<Path, Boolean>> res = command.getBestRecordingForName(HOST_ID, "foo");
 
-            // Assertions.assertFalse(res.getStream().isPresent());
-            // Assertions.assertFalse(res.getConnection().isPresent());
+            Assertions.assertFalse(res.isPresent());
         }
 
         @Test
@@ -245,12 +243,9 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(fs.isRegularFile(rec)).thenReturn(true);
             Mockito.when(fs.isReadable(rec)).thenReturn(false);
 
-            // FIXME
-            // UploadRecordingCommand.RecordingConnection res =
-            //         command.getBestRecordingForName(HOST_ID, "foo");
+            Optional<Pair<Path, Boolean>> res = command.getBestRecordingForName(HOST_ID, "foo");
 
-            // Assertions.assertFalse(res.getStream().isPresent());
-            // Assertions.assertFalse(res.getConnection().isPresent());
+            Assertions.assertFalse(res.isPresent());
         }
     }
 
@@ -288,29 +283,36 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(conn.getService()).thenReturn(svc);
             Mockito.when(svc.getAvailableRecordings()).thenReturn(List.of(rec));
             Mockito.when(rec.getName()).thenReturn("foo");
-            Mockito.when(command.targetConnectionManager.connect(Mockito.anyString()))
-                    .thenReturn(conn);
             Mockito.when(svc.openStream(Mockito.any(), Mockito.anyBoolean())).thenReturn(stream);
 
-            CloseableHttpResponse httpResp = Mockito.mock(CloseableHttpResponse.class);
-            HttpEntity entity = Mockito.mock(HttpEntity.class);
-            StatusLine status = Mockito.mock(StatusLine.class);
-            Mockito.when(httpClient.execute(Mockito.any())).thenReturn(httpResp);
-            Mockito.when(httpResp.getEntity()).thenReturn(entity);
-            Mockito.when(httpResp.getStatusLine()).thenReturn(status);
-            Mockito.when(status.toString()).thenReturn("status_line");
-            Mockito.when(entity.getContent())
-                    .thenReturn(new ByteArrayInputStream("entity_response".getBytes()));
+            HttpRequest<Buffer> req = Mockito.mock(HttpRequest.class);
+            HttpResponse<Buffer> resp = Mockito.mock(HttpResponse.class);
+            Mockito.when(webClient.postAbs(Mockito.anyString())).thenReturn(req);
+            Mockito.doAnswer(
+                            new Answer<Void>() {
+                                @Override
+                                public Void answer(InvocationOnMock args) throws Throwable {
+                                    AsyncResult<HttpResponse<Buffer>> asyncResult =
+                                            Mockito.mock(AsyncResult.class);
+                                    Mockito.when(asyncResult.result()).thenReturn(resp);
+                                    Mockito.when(resp.statusCode()).thenReturn(200);
+                                    Mockito.when(resp.statusMessage()).thenReturn("OK");
+                                    Mockito.when(resp.bodyAsString()).thenReturn("HELLO");
+                                    ((Handler<AsyncResult<HttpResponse<Buffer>>>)
+                                                    args.getArgument(1))
+                                            .handle(asyncResult);
+                                    return null;
+                                }
+                            })
+                    .when(req)
+                    .sendMultipartForm(Mockito.any(), Mockito.any());
 
             command.execute(new String[] {HOST_ID, "foo", UPLOAD_URL});
 
-            ArgumentCaptor<HttpUriRequest> captor = ArgumentCaptor.forClass(HttpUriRequest.class);
-            Mockito.verify(httpClient).execute(captor.capture());
-            MatcherAssert.assertThat(captor.getValue(), Matchers.instanceOf(HttpPost.class));
-            HttpPost post = (HttpPost) captor.getValue();
-            MatcherAssert.assertThat(post.getURI().toString(), Matchers.equalTo(UPLOAD_URL));
-            MatcherAssert.assertThat(post.getEntity(), Matchers.notNullValue());
-            Mockito.verify(cw).println("[status_line] entity_response");
+            ArgumentCaptor<String> urlCaptor = ArgumentCaptor.forClass(String.class);
+            Mockito.verify(webClient).postAbs(urlCaptor.capture());
+            MatcherAssert.assertThat(urlCaptor.getValue(), Matchers.equalTo(UPLOAD_URL));
+            Mockito.verify(cw).println("[200 OK] HELLO");
         }
     }
 
@@ -348,19 +350,29 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
             Mockito.when(conn.getService()).thenReturn(svc);
             Mockito.when(svc.getAvailableRecordings()).thenReturn(List.of(rec));
             Mockito.when(rec.getName()).thenReturn("foo");
-            Mockito.when(command.targetConnectionManager.connect(Mockito.anyString()))
-                    .thenReturn(conn);
             Mockito.when(svc.openStream(Mockito.any(), Mockito.anyBoolean())).thenReturn(stream);
 
-            CloseableHttpResponse httpResp = Mockito.mock(CloseableHttpResponse.class);
-            HttpEntity entity = Mockito.mock(HttpEntity.class);
-            StatusLine status = Mockito.mock(StatusLine.class);
-            Mockito.when(httpClient.execute(Mockito.any())).thenReturn(httpResp);
-            Mockito.when(httpResp.getEntity()).thenReturn(entity);
-            Mockito.when(httpResp.getStatusLine()).thenReturn(status);
-            Mockito.when(status.toString()).thenReturn("status_line");
-            Mockito.when(entity.getContent())
-                    .thenReturn(new ByteArrayInputStream("entity_response".getBytes()));
+            HttpRequest<Buffer> req = Mockito.mock(HttpRequest.class);
+            HttpResponse<Buffer> resp = Mockito.mock(HttpResponse.class);
+            Mockito.when(webClient.postAbs(Mockito.anyString())).thenReturn(req);
+            Mockito.doAnswer(
+                            new Answer<Void>() {
+                                @Override
+                                public Void answer(InvocationOnMock args) throws Throwable {
+                                    AsyncResult<HttpResponse<Buffer>> asyncResult =
+                                            Mockito.mock(AsyncResult.class);
+                                    Mockito.when(asyncResult.result()).thenReturn(resp);
+                                    Mockito.when(resp.statusCode()).thenReturn(200);
+                                    Mockito.when(resp.statusMessage()).thenReturn("OK");
+                                    Mockito.when(resp.bodyAsString()).thenReturn("HELLO");
+                                    ((Handler<AsyncResult<HttpResponse<Buffer>>>)
+                                                    args.getArgument(1))
+                                            .handle(asyncResult);
+                                    return null;
+                                }
+                            })
+                    .when(req)
+                    .sendMultipartForm(Mockito.any(), Mockito.any());
 
             Output<?> out = command.serializableExecute(new String[] {HOST_ID, "foo", UPLOAD_URL});
 
@@ -370,16 +382,12 @@ class UploadRecordingCommandTest implements ValidatesTargetId, ValidatesRecordin
                     Matchers.allOf(
                             Matchers.startsWith("{"),
                             Matchers.endsWith("}"),
-                            Matchers.containsString("body=entity_response"),
-                            Matchers.containsString("status=status_line"),
-                            Matchers.containsString(", ")));
+                            Matchers.containsString("body=HELLO"),
+                            Matchers.containsString("status=200 OK")));
 
-            ArgumentCaptor<HttpUriRequest> captor = ArgumentCaptor.forClass(HttpUriRequest.class);
-            Mockito.verify(httpClient).execute(captor.capture());
-            MatcherAssert.assertThat(captor.getValue(), Matchers.instanceOf(HttpPost.class));
-            HttpPost post = (HttpPost) captor.getValue();
-            MatcherAssert.assertThat(post.getURI().toString(), Matchers.equalTo(UPLOAD_URL));
-            MatcherAssert.assertThat(post.getEntity(), Matchers.notNullValue());
+            ArgumentCaptor<String> urlCaptor = ArgumentCaptor.forClass(String.class);
+            Mockito.verify(webClient).postAbs(urlCaptor.capture());
+            MatcherAssert.assertThat(urlCaptor.getValue(), Matchers.equalTo(UPLOAD_URL));
         }
     }
 }
