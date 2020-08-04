cf0ee0032fc12a228583653e79aae748ece1ba5b
Andrew Azores <me@andrewazor.es>
2020-08-04 17:55:55 -0400

    Avoid Spotbugs warning
    Fixes #226

--- src/main/java/com/redhat/rhjmc/containerjfr/platform/internal/PlatformDetectionStrategy.java
+++ src/main/java/com/redhat/rhjmc/containerjfr/platform/internal/PlatformDetectionStrategy.java
@@ -44,8 +44,7 @@ package com.redhat.rhjmc.containerjfr.platform.internal;
 import com.redhat.rhjmc.containerjfr.net.AuthManager;
 import com.redhat.rhjmc.containerjfr.platform.PlatformClient;
 
-public interface PlatformDetectionStrategy<T extends PlatformClient>
-        extends Comparable<PlatformDetectionStrategy<?>> {
+public interface PlatformDetectionStrategy<T extends PlatformClient> {
     int PRIORITY_DEFAULT = 0;
     int PRIORITY_PLATFORM = 50;
 
@@ -56,9 +55,4 @@ public interface PlatformDetectionStrategy<T extends PlatformClient>
     T getPlatformClient();
 
     AuthManager getAuthManager();
-
-    @Override
-    default int compareTo(PlatformDetectionStrategy<?> o) {
-        return Integer.compare(o.getPriority(), getPriority());
-    }
 }
