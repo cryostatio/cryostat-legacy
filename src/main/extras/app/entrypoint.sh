de6068d5eb75daf8ea0ad2f8dccb31bbd6a176b7
Andrew Azores <me@andrewazor.es>
2020-08-18 18:03:17 -0400

    Add self-signed cert to copied system truststore

--- src/main/extras/app/entrypoint.sh
+++ src/main/extras/app/entrypoint.sh
@@ -26,6 +26,8 @@ function createJmxCredentials() {
 SSL_KEYSTORE="/tmp/keystore.jks"
 SSL_KEY_PASS="$(genpass)"
 SSL_STORE_PASS="$SSL_KEY_PASS"
+SSL_TRUSTSTORE="/tmp/cacerts"
+# SSL_TRUSTSTORE_PASS="$(genpass)"
 function createSslStores() {
     pushd /tmp
 
@@ -38,19 +40,23 @@ function createSslStores() {
         -storepass "$SSL_STORE_PASS" \
         -keystore "$SSL_KEYSTORE"
 
-    keytool -export \
+    keytool -exportcert \
         -alias container-jfr \
         -keystore "$SSL_KEYSTORE" \
         -storepass "$SSL_STORE_PASS" \
         -file server.cer
 
-    keytool -import -v \
+    cp /usr/lib/jvm/java-11-openjdk/lib/security/cacerts "$SSL_TRUSTSTORE"
+    chown jboss:root "$SSL_TRUSTSTORE"
+    chmod 640 "$SSL_TRUSTSTORE"
+
+    keytool -importcert -v \
         -noprompt \
         -trustcacerts \
+        -keystore "$SSL_TRUSTSTORE" \
         -alias selftrust \
         -file server.cer \
-        -keypass "$SSL_KEY_PASS" \
-        -storepass "$SSL_STORE_PASS"
+        -storepass changeit
 
     popd
 }
@@ -88,6 +94,8 @@ if [ "$CONTAINER_JFR_RJMX_AUTH" = "true" ] || [ -n "$CONTAINER_JFR_RJMX_USER" ] 
     FLAGS+=("-Dcom.sun.management.jmxremote.ssl.need.client.auth=false")
     FLAGS+=("-Djavax.net.ssl.keyStore=$SSL_KEYSTORE")
     FLAGS+=("-Djavax.net.ssl.keyStorePassword=$SSL_KEY_PASS")
+    FLAGS+=("-Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE")
+    FLAGS+=("-Djavax.net.ssl.trustStorePassword=changeit")
 else
     FLAGS+=("-Dcom.sun.management.jmxremote.authenticate=false")
     FLAGS+=("-Dcom.sun.management.jmxremote.ssl=false")
