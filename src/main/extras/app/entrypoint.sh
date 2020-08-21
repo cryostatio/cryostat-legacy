5040a7529f7baa5d2d638e769177dbffa106dbd5
Andrew Azores <me@andrewazor.es>
2020-08-21 10:29:16 -0400

    Always enable SSL, even when auth credentials are disabled
    Always import additional trusted SSL certs, and generate self-signed
    certificate for encrypting own connections, even when JMX user
    authentication/credentials are not configured

--- src/main/extras/app/entrypoint.sh
+++ src/main/extras/app/entrypoint.sh
@@ -31,35 +31,12 @@ SSL_TRUSTSTORE_PASS="$(genpass)"
 function createSslStores() {
     pushd /tmp
 
-    keytool -genkeypair -v \
-        -alias container-jfr \
-        -dname "cn=container-jfr, o=Red Hat, c=US" \
-        -validity 180 \
-        -keyalg RSA \
-        -keypass "$SSL_KEY_PASS" \
-        -storepass "$SSL_STORE_PASS" \
-        -keystore "$SSL_KEYSTORE"
-
-    keytool -exportcert -v \
-        -alias container-jfr \
-        -keystore "$SSL_KEYSTORE" \
-        -storepass "$SSL_STORE_PASS" \
-        -file server.cer
-
     keytool -importkeystore \
         -noprompt \
         -srckeystore /usr/lib/jvm/java-11-openjdk/lib/security/cacerts \
         -srcstorepass changeit \
         -destkeystore "$SSL_TRUSTSTORE" \
         -deststorepass "$SSL_TRUSTSTORE_PASS"
-
-    keytool -importcert -v \
-        -noprompt \
-        -trustcacerts \
-        -keystore "$SSL_TRUSTSTORE" \
-        -alias selftrust \
-        -file server.cer \
-        -storepass "$SSL_TRUSTSTORE_PASS"
 
     popd
 }
@@ -87,6 +64,35 @@ function importTrustStores() {
     done
 }
 
+function generateSslCert() {
+    pushd /tmp
+
+    keytool -genkeypair -v \
+        -alias container-jfr \
+        -dname "cn=container-jfr, o=Red Hat, c=US" \
+        -validity 180 \
+        -keyalg RSA \
+        -keypass "$SSL_KEY_PASS" \
+        -storepass "$SSL_STORE_PASS" \
+        -keystore "$SSL_KEYSTORE"
+
+    keytool -exportcert -v \
+        -alias container-jfr \
+        -keystore "$SSL_KEYSTORE" \
+        -storepass "$SSL_STORE_PASS" \
+        -file server.cer
+
+    keytool -importcert -v \
+        -noprompt \
+        -trustcacerts \
+        -keystore "$SSL_TRUSTSTORE" \
+        -alias selftrust \
+        -file server.cer \
+        -storepass "$SSL_TRUSTSTORE_PASS"
+
+    popd
+}
+
 if [ -z "$CONTAINER_JFR_RJMX_PORT" ]; then
     CONTAINER_JFR_RJMX_PORT=9091
 fi
@@ -100,6 +106,10 @@ FLAGS=(
     "-Dcom.sun.management.jmxremote.autodiscovery=true"
     "-Dcom.sun.management.jmxremote.port=$CONTAINER_JFR_RJMX_PORT"
     "-Dcom.sun.management.jmxremote.rmi.port=$CONTAINER_JFR_RMI_PORT"
+    "-Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE"
+    "-Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASS"
+    "-Djavax.net.ssl.keyStore=$SSL_KEYSTORE"
+    "-Djavax.net.ssl.keyStorePassword=$SSL_KEY_PASS"
 )
 
 if [ -z "$CONTAINER_JFR_RJMX_AUTH" ]; then
@@ -107,11 +117,12 @@ if [ -z "$CONTAINER_JFR_RJMX_AUTH" ]; then
     CONTAINER_JFR_RJMX_AUTH=true
 fi
 
+createSslStores
+importTrustStores
+generateSslCert
 if [ "$CONTAINER_JFR_RJMX_AUTH" = "true" ] || [ -n "$CONTAINER_JFR_RJMX_USER" ] ||
     [ -n "$CONTAINER_JFR_RJMX_PASS" ]; then
     createJmxCredentials
-    createSslStores
-    importTrustStores
 
     FLAGS+=("-Dcom.sun.management.jmxremote.authenticate=true")
     FLAGS+=("-Dcom.sun.management.jmxremote.password.file=$PWFILE")
@@ -119,10 +130,6 @@ if [ "$CONTAINER_JFR_RJMX_AUTH" = "true" ] || [ -n "$CONTAINER_JFR_RJMX_USER" ] 
     FLAGS+=("-Dcom.sun.management.jmxremote.ssl=true")
     FLAGS+=("-Dcom.sun.management.jmxremote.registry.ssl=true")
     FLAGS+=("-Dcom.sun.management.jmxremote.ssl.need.client.auth=true")
-    FLAGS+=("-Djavax.net.ssl.keyStore=$SSL_KEYSTORE")
-    FLAGS+=("-Djavax.net.ssl.keyStorePassword=$SSL_KEY_PASS")
-    FLAGS+=("-Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE")
-    FLAGS+=("-Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASS")
 else
     FLAGS+=("-Dcom.sun.management.jmxremote.authenticate=false")
     FLAGS+=("-Dcom.sun.management.jmxremote.ssl=false")
