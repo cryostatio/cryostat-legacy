01a2c3730edd580b8a76ebcb2398e7d80c23966d
Victor Ma <vma@redhat.com>
2020-11-13 13:09:26 -0500

    Add `TargetRecordingOptionsListGetHandler` (#329)
    * Add TargetRecordingOptionsListGetHandler
    
    * Add tests
    
    * Use List instead of Map
    
    * Add docs

---
+++ src/test/java/com/redhat/rhjmc/containerjfr/net/web/http/api/v2/TargetRecordingOptionsListGetHandlerTest.java
@@ -0,0 +1,146 @@
+/*-
+ * #%L
+ * Container JFR
+ * %%
+ * Copyright (C) 2020 Red Hat, Inc.
+ * %%
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * #L%
+ */
+package com.redhat.rhjmc.containerjfr.net.web.http.api.v2;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.openjdk.jmc.common.unit.IOptionDescriptor;
+import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import com.redhat.rhjmc.containerjfr.MainModule;
+import com.redhat.rhjmc.containerjfr.core.log.Logger;
+import com.redhat.rhjmc.containerjfr.core.net.JFRConnection;
+import com.redhat.rhjmc.containerjfr.jmc.serialization.SerializableOptionDescriptor;
+import com.redhat.rhjmc.containerjfr.net.AuthManager;
+import com.redhat.rhjmc.containerjfr.net.ConnectionDescriptor;
+import com.redhat.rhjmc.containerjfr.net.TargetConnectionManager;
+import com.redhat.rhjmc.containerjfr.net.TargetConnectionManager.ConnectedTask;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
+
+@ExtendWith(MockitoExtension.class)
+class TargetRecordingOptionsListGetHandlerTest {
+
+    TargetRecordingOptionsListGetHandler handler;
+    @Mock AuthManager auth;
+    @Mock TargetConnectionManager targetConnectionManager;
+    @Mock IFlightRecorderService service;
+    @Mock JFRConnection connection;
+    @Mock Logger logger;
+    Gson gson = MainModule.provideGson(logger);
+
+    @BeforeEach
+    void setup() {
+        this.handler =
+                new TargetRecordingOptionsListGetHandler(auth, targetConnectionManager, gson);
+    }
+
+    @Test
+    void shouldHandleGETRequest() {
+        MatcherAssert.assertThat(handler.httpMethod(), Matchers.equalTo(HttpMethod.GET));
+    }
+
+    @Test
+    void shouldHandleCorrectPath() {
+        MatcherAssert.assertThat(
+                handler.path(), Matchers.equalTo("/api/v2/targets/:targetId/recordingOptionsList"));
+    }
+
+    @Test
+    void shouldRespondWithRecordingOptionsList() throws Exception {
+        IOptionDescriptor<String> descriptor = mock(IOptionDescriptor.class);
+        when(descriptor.getName()).thenReturn("foo");
+        when(descriptor.getDescription()).thenReturn("Foo Option");
+        when(descriptor.getDefault()).thenReturn("bar");
+        Map<String, IOptionDescriptor<?>> options = Map.of("foo-option", descriptor);
+
+        when(targetConnectionManager.executeConnectedTask(
+                        Mockito.any(ConnectionDescriptor.class), Mockito.any()))
+                .thenAnswer(
+                        arg0 -> ((ConnectedTask<Object>) arg0.getArgument(1)).execute(connection));
+        when(connection.getService()).thenReturn(service);
+        when(service.getAvailableRecordingOptions()).thenReturn(options);
+
+        RoutingContext ctx = Mockito.mock(RoutingContext.class);
+        HttpServerResponse resp = Mockito.mock(HttpServerResponse.class);
+        Mockito.when(ctx.response()).thenReturn(resp);
+        Mockito.when(ctx.pathParam("targetId")).thenReturn("foo:9091");
+        HttpServerRequest req = Mockito.mock(HttpServerRequest.class);
+        Mockito.when(ctx.request()).thenReturn(req);
+        Mockito.when(req.headers()).thenReturn(MultiMap.caseInsensitiveMultiMap());
+
+        handler.handleAuthenticated(ctx);
+
+        ArgumentCaptor<String> responseCaptor = ArgumentCaptor.forClass(String.class);
+        Mockito.verify(resp).end(responseCaptor.capture());
+        List<SerializableOptionDescriptor> result =
+                gson.fromJson(
+                        responseCaptor.getValue(),
+                        new TypeToken<List<SerializableOptionDescriptor>>() {}.getType());
+
+        MatcherAssert.assertThat(
+                result,
+                Matchers.equalTo(Arrays.asList(new SerializableOptionDescriptor(descriptor))));
+    }
+}
