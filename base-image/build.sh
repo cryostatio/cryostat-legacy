a8083922dc7f49bd2be1d5be40f068fe5c976c77
Andrew Azores <aazores@redhat.com>
2020-11-19 18:04:02 -0500

    Make base image name configurable

--- base-image/build.sh
+++ base-image/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "$IMAGE" ]; then
+    IMAGE="quay.io/rh-jmc-team/container-jfr-base"
+fi
+
 if [ -z "$TAG" ]; then
     TAG="0.1.0"
 fi
@@ -7,8 +11,6 @@ fi
 if [ -z "$BUILDER" ]; then
     BUILDER="podman"
 fi
-
-IMAGE="quay.io/rh-jmc-team/container-jfr-base"
 
 $BUILDER build -t $IMAGE:$TAG -f "$(dirname $0)"/Containerfile
 $BUILDER tag $IMAGE:$TAG $IMAGE:latest
