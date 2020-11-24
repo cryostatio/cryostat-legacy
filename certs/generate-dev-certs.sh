8e2c6b7e45529244bac84f8e7bdcdd1c84cc9242
Andrew Azores <aazores@redhat.com>
2020-11-24 15:55:31 -0500

    Add clean/generate arg handling

--- certs/generate-dev-certs.sh
+++ certs/generate-dev-certs.sh
@@ -1,30 +1,46 @@
 #!/bin/sh
 
 set -x
+
+CERTS_DIR=$(dirname $0)
+
+SSL_KEYSTORE=container-jfr-keystore.p12
+
+SSL_TRUSTSTORE=container-jfr-truststore.p12
+
+SSL_KEYSTORE_PASS_FILE=keystore.pass
+
+function cleanup() {
+    pushd "$CERTS_DIR"
+    rm $SSL_TRUSTSTORE $SSL_KEYSTORE $SSL_KEYSTORE_PASS_FILE
+    popd
+}
+
+case "$1" in
+    clean)
+        cleanup
+        exit 0
+        ;;
+    generate)
+        ;;
+    *)
+        echo "Usage: $0 [clean|generate]"
+        exit 1
+        ;;
+esac
+
 set -e
 
 function genpass() {
     echo "$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)"
 }
 
-CERTS_DIR=$(dirname $0)
-
-SSL_TRUSTSTORE=container-jfr-truststore.p12
-
 SSL_TRUSTSTORE_PASS=$(genpass)
-
-SSL_KEYSTORE=container-jfr-keystore.p12
 
 SSL_KEYSTORE_PASS=$(genpass)
 
-SSL_KEYSTORE_PASS_FILE=keystore.pass
-
 pushd $CERTS_DIR
 trap popd EXIT
-
-set +e
-rm $SSL_TRUSTSTORE $SSL_KEYSTORE $SSL_KEYSTORE_PASS_FILE
-set -e
 
 echo "$SSL_KEYSTORE_PASS" > $SSL_KEYSTORE_PASS_FILE
 
