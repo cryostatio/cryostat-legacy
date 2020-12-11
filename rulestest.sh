8e5a6e32d8d23ecc51ff69e7fa8d7a0cbbc8b0c8
Andrew Azores <aazores@redhat.com>
2020-12-11 16:20:20 -0500

    Automate add/remove demo app

--- rulestest.sh
+++ rulestest.sh
@@ -2,6 +2,12 @@
 
 # FIXME this should be an integration test
 
+# run smoketest.sh first in a separate terminal, then this once the ContainerJFR instance has finished startup
+
+# FIXME once RuleProcessor can apply new rules to existing targets, remove podman commands
+
+podman kill vertx-fib-demo
+
 demoAppServiceUrl="service:jmx:rmi:///jndi/rmi://container-jfr:9093/jmxrmi"
 
 curl -vLk \
@@ -20,3 +26,8 @@ curl -vLk \
     -F archivalPeriodSeconds="60" \
     -F preservedArchives="3" \
     https://0.0.0.0:8181/api/v2/rules
+
+podman run \
+    --name vertx-fib-demo \
+    --pod container-jfr \
+    --rm -d quay.io/andrewazores/vertx-fib-demo:0.4.0
