27a7078bbd6bbecb9a76770df4ab60d2379a2193
Andrew Azores <aazores@redhat.com>
2020-12-14 17:28:20 -0500

    Fix periodic archiver recording name

--- src/main/java/io/cryostat/rules/PeriodicArchiver.java
+++ src/main/java/io/cryostat/rules/PeriodicArchiver.java
@@ -96,7 +96,8 @@ class PeriodicArchiver implements Runnable {
 
     @Override
     public void run() {
-        logger.trace(String.format("PeriodicArchiver for %s running", rule.getName()));
+        // FIXME auto_ prefix should not be hardcoded
+        logger.trace(String.format("PeriodicArchiver for %s running", "auto_" + rule.getName()));
 
         try {
             performArchival();
@@ -126,7 +127,9 @@ class PeriodicArchiver implements Runnable {
                                                         serviceRef.getJMXServiceUrl().toString()))
                                         .replaceAll(
                                                 ":recordingName",
-                                                URLEncodedUtils.formatSegments(rule.getName())))
+                                                // FIXME auto_ prefix should not be hardcoded
+                                                URLEncodedUtils.formatSegments(
+                                                        "auto_" + rule.getName())))
                         .normalize();
         MultiMap headers = MultiMap.caseInsensitiveMultiMap();
         if (credentials != null) {
