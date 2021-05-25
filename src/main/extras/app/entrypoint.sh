1c7b529dd0e14b32f684674148cee61b9784256f
Andrew Azores <aazores@redhat.com>
2021-05-25 14:49:12 -0400

    Set client lib path by env var

--- src/main/extras/app/entrypoint.sh
+++ src/main/extras/app/entrypoint.sh
@@ -150,13 +150,16 @@ if [ -n "$CRYOSTAT_JUL_CONFIG" ]; then
     FLAGS+=("-Djava.util.logging.config.file=$CRYOSTAT_JUL_CONFIG")
 fi
 
-export CRYOSTAT_CLIENT_LIB="/clientlib"
+CLASSPATH="/app/resources:/app/classes:/app/libs/*"
+if [ -n "CRYOSTAT_CLIENTLIB_PATH" ]; then
+    CLASSPATH="$CLASSPATH:$CRYOSTAT_CLIENTLIB_PATH/*"
+fi
 
 export KEYSTORE_PATH
 export KEYSTORE_PASS
 export SSL_TRUSTSTORE_DIR
 exec java \
     "${FLAGS[@]}" \
-    -cp /app/resources:/app/classes:/app/libs/*:"$CRYOSTAT_CLIENT_LIB/*" \
+    -cp "$CLASSPATH" \
     io.cryostat.Cryostat \
     "$@"
