b88bf8c42e1a0d8d805f582568b453e6667fc596
Hareet Dhillon <hdhillon@redhat.com>
2021-06-21 17:24:34 -0400

    Accomplish the same using system properties

--- src/test/java/itest/ClientAssetsIT.java
+++ src/test/java/itest/ClientAssetsIT.java
@@ -49,7 +49,9 @@ import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
+@DisabledIfSystemProperty(named = "isMinimalBuild", matches = "true")
 public class ClientAssetsIT extends TestBase {
 
     static File file;
