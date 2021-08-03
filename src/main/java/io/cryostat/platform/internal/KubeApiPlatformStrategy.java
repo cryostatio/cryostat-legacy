75b94705466f1eb76d4838fd61a644c5516d04f8
Andrew Azores <aazores@redhat.com>
2021-08-03 14:18:20 -0400

    fix(platform): lower log level for k8s namespace read failure

--- src/main/java/io/cryostat/platform/internal/KubeApiPlatformStrategy.java
+++ src/main/java/io/cryostat/platform/internal/KubeApiPlatformStrategy.java
@@ -118,7 +118,7 @@ class KubeApiPlatformStrategy implements PlatformDetectionStrategy<KubeApiPlatfo
         try {
             return fs.readString(Paths.get(Config.KUBERNETES_NAMESPACE_PATH));
         } catch (IOException e) {
-            logger.info(e);
+            logger.trace(e);
             return null;
         }
     }
