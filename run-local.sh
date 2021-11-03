858ea1e94a3447c70d9eeb096c14332811df0511
Andrew Azores <aazores@redhat.com>
2021-11-03 13:53:04 -0400

    feat(hotreload): create configuration for hot-reload dev server

--- run-local.sh
+++ run-local.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
+script_dir="$(dirname $(realpath $0))"
+
 if [ -z $MVN ]; then
     MVN=$(which mvn)
 fi
 
-if [ ! -f target/classes/io/cryostat/Cryostat.class ]; then
-    echo "Ensure you have done 'mvn prepare-package' before running this script"
-    exit 1
-fi
-
-script_dir="$(dirname $(realpath $0))"
+for i in archive clientlib conf templates; do
+    if [ -e $i ]; then
+        if [ ! -d $i ]; then
+            echo "$i already exists but is not a directory"
+            exit 1
+        fi
+    else
+        mkdir $i
+    fi
+done
 
 flags=(
-    "-Dvertxweb.environment=dev"
     "-Dcom.sun.management.jmxremote.autodiscovery=true"
     "-Dcom.sun.management.jmxremote.port=9091"
     "-Dcom.sun.management.jmxremote.rmi.port=9091"
@@ -25,11 +30,12 @@ MAVEN_OPTS="${flags[@]}" \
     CRYOSTAT_PLATFORM=io.cryostat.platform.internal.DefaultPlatformStrategy \
     CRYOSTAT_DISABLE_SSL=true \
     CRYOSTAT_DISABLE_JMX_AUTH=true \
-    CRYOSTAT_WEB_HOST=$(hostname) \
-    CRYOSTAT_WEB_PORT=8080 \
+    CRYOSTAT_WEB_HOST=localhost \
+    CRYOSTAT_WEB_PORT=8181 \
+    CRYOSTAT_CORS_ORIGIN=http://localhost:9000 \
     CRYOSTAT_AUTH_MANAGER=io.cryostat.net.NoopAuthManager \
     CRYOSTAT_ARCHIVE_PATH="$script_dir/archive" \
     CRYOSTAT_CLIENTLIB_PATH="$script_dir/clientlib" \
     CRYOSTAT_CONFIG_PATH="$script_dir/conf" \
     CRYOSTAT_TEMPLATE_PATH="$script_dir/templates" \
-    $MVN exec:java
+    $MVN vertx:run
