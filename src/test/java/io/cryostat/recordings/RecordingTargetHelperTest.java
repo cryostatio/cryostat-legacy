973267d42b7dd6b0556434a9c105cd9f69dbc961
Hareet Dhillon <hdhillon@redhat.com>
2021-11-03 17:06:30 -0400

    Update TargetRecordingDeleteHandlerTest to reflect extraction of the actual delete operation into RecordingTargetHelper; create RecordingTargetHelperTest

---
+++ src/test/java/io/cryostat/recordings/RecordingTargetHelperTest.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright The Cryostat Authors
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.cryostat.recordings;
+
+import static org.mockito.Mockito.lenient;
+
+import java.util.List;
+import java.util.Map;
+
+import org.openjdk.jmc.common.unit.IQuantity;
+import org.openjdk.jmc.common.unit.QuantityConversionException;
+import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
+import org.openjdk.jmc.rjmx.services.jfr.IRecordingDescriptor;
+
+import io.cryostat.core.log.Logger;
+import io.cryostat.core.net.JFRConnection;
+import io.cryostat.messaging.notifications.Notification;
+import io.cryostat.messaging.notifications.NotificationFactory;
+import io.cryostat.net.AuthManager;
+import io.cryostat.net.ConnectionDescriptor;
+import io.cryostat.net.TargetConnectionManager;
+import io.cryostat.net.reports.ReportService;
+import io.cryostat.net.web.http.HttpMimeType;
+import io.cryostat.recordings.RecordingTargetHelper;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+@ExtendWith(MockitoExtension.class)
+public class RecordingTargetHelperTest {
+    RecordingTargetHelper recordingTargetHelper;
+    @Mock AuthManager auth;
+    @Mock TargetConnectionManager targetConnectionManager;
+    @Mock EventOptionsBuilder.Factory eventOptionsBuilderFactory;
+    @Mock NotificationFactory notificationFactory;
+    @Mock Notification notification;
+    @Mock Notification.Builder notificationBuilder;
+    @Mock ReportService reportService;
+    @Mock Logger logger;
+
+    @Mock JFRConnection connection;
+    @Mock IFlightRecorderService service;
+
+    @BeforeEach
+    void setup() {
+        lenient().when(notificationFactory.createBuilder()).thenReturn(notificationBuilder);
+        lenient()
+                .when(notificationBuilder.metaCategory(Mockito.any()))
+                .thenReturn(notificationBuilder);
+        lenient()
+                .when(notificationBuilder.metaType(Mockito.any(Notification.MetaType.class)))
+                .thenReturn(notificationBuilder);
+        lenient()
+                .when(notificationBuilder.metaType(Mockito.any(HttpMimeType.class)))
+                .thenReturn(notificationBuilder);
+        lenient().when(notificationBuilder.message(Mockito.any())).thenReturn(notificationBuilder);
+        lenient().when(notificationBuilder.build()).thenReturn(notification);
+        this.recordingTargetHelper = new RecordingTargetHelper(targetConnectionManager, eventOptionsBuilderFactory, notificationFactory, reportService, logger);
+    }
+
+    /* From: TargetRecordingDeleteHandlerTest */
+
+    @Test
+    void shouldDeleteRecording() throws Exception {
+        ConnectionDescriptor connectionDescriptor = new ConnectionDescriptor("fooTarget");
+        String recordingName = "someRecording";
+
+        Mockito.when(targetConnectionManager.executeConnectedTask(Mockito.any(), Mockito.any()))
+                .thenAnswer(
+                        new Answer() {
+                            @Override
+                            public Object answer(InvocationOnMock invocation) throws Throwable {
+                                TargetConnectionManager.ConnectedTask task =
+                                        (TargetConnectionManager.ConnectedTask)
+                                                invocation.getArgument(1);
+                                return task.execute(connection);
+                            }
+                        });
+
+        Mockito.when(connection.getService()).thenReturn(service);
+        IRecordingDescriptor descriptor = createDescriptor(recordingName);
+        Mockito.when(service.getAvailableRecordings()).thenReturn(List.of(descriptor));
+
+       recordingTargetHelper.deleteRecording(connectionDescriptor, recordingName);
+
+        Mockito.verify(service).close(descriptor);
+        Mockito.verify(reportService)
+                .delete(
+                        Mockito.argThat(
+                                arg ->
+                                        arg.getTargetId()
+                                                .equals(connectionDescriptor.getTargetId())),
+                        Mockito.eq(recordingName));
+
+        Mockito.verify(notificationFactory).createBuilder();
+        Mockito.verify(notificationBuilder).metaCategory("RecordingDeleted");
+        Mockito.verify(notificationBuilder).metaType(HttpMimeType.JSON);
+        Mockito.verify(notificationBuilder)
+                .message(Map.of("recording", "someRecording", "target", "fooTarget"));
+        Mockito.verify(notificationBuilder).build();
+        Mockito.verify(notification).send();
+    }
+
+    @Test
+    void shouldHandleRecordingNotFound() throws Exception {
+        ConnectionDescriptor connectionDescriptor = new ConnectionDescriptor("fooTarget");
+        String recordingName = "someRecording";
+
+        Mockito.when(targetConnectionManager.executeConnectedTask(Mockito.any(), Mockito.any()))
+                .thenAnswer(
+                        new Answer() {
+                            @Override
+                            public Object answer(InvocationOnMock invocation) throws Throwable {
+                                TargetConnectionManager.ConnectedTask task =
+                                        (TargetConnectionManager.ConnectedTask)
+                                                invocation.getArgument(1);
+                                return task.execute(connection);
+                            }
+                        });
+
+        Mockito.when(connection.getService()).thenReturn(service);
+        Mockito.when(service.getAvailableRecordings()).thenReturn(List.of());
+
+        RecordingNotFoundException ex =
+                Assertions.assertThrows(
+                        RecordingNotFoundException.class, () -> recordingTargetHelper.deleteRecording(connectionDescriptor, recordingName));
+        MatcherAssert.assertThat(ex.getTargetId(), Matchers.equalTo(connectionDescriptor.getTargetId()));
+        MatcherAssert.assertThat(ex.getRecordingName(), Matchers.equalTo(recordingName));
+    }
+
+    private static IRecordingDescriptor createDescriptor(String name)
+            throws QuantityConversionException {
+        IQuantity zeroQuantity = Mockito.mock(IQuantity.class);
+        IRecordingDescriptor descriptor = Mockito.mock(IRecordingDescriptor.class);
+        Mockito.lenient().when(descriptor.getId()).thenReturn(1L);
+        Mockito.lenient().when(descriptor.getName()).thenReturn(name);
+        Mockito.lenient()
+                .when(descriptor.getState())
+                .thenReturn(IRecordingDescriptor.RecordingState.STOPPED);
+        Mockito.lenient().when(descriptor.getStartTime()).thenReturn(zeroQuantity);
+        Mockito.lenient().when(descriptor.getDuration()).thenReturn(zeroQuantity);
+        Mockito.lenient().when(descriptor.isContinuous()).thenReturn(false);
+        Mockito.lenient().when(descriptor.getToDisk()).thenReturn(false);
+        Mockito.lenient().when(descriptor.getMaxSize()).thenReturn(zeroQuantity);
+        Mockito.lenient().when(descriptor.getMaxAge()).thenReturn(zeroQuantity);
+        return descriptor;
+    }
+
+}
