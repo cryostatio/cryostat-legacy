aad5dce6d04be8fe27d3f75ed9caf8283f09c732
Andrew Azores <aazores@redhat.com>
2021-12-02 12:29:48 -0500

    style(itest): quote variables

--- repeated-integration-tests.sh
+++ repeated-integration-tests.sh
@@ -2,14 +2,14 @@
 
 failures=0
 numeric='^[0-9]+$'
-if [[ "$1" =~ $numeric ]]; then
-    runs=$1
+if [[ "$1" =~ "${numeric}" ]]; then
+    runs="$1"
 else
     runs=50
 fi
 
 function cleanup() {
-    runcount=$runs
+    runcount="${runs}"
     exit 1
 }
 trap cleanup EXIT
@@ -17,7 +17,7 @@ trap cleanup EXIT
 logfile="cryostat-itests-$(date -Iminutes).log"
 
 runcount=0
-while [ $runcount -lt $runs ]; do
+while [ "${runcount}" -lt "${runs}" ]; do
     mvn \
         exec:exec@create-pod \
         exec:exec@start-jfr-datasource \
@@ -28,7 +28,7 @@ while [ $runcount -lt $runs ]; do
         exec:exec@stop-jfr-datasource \
         exec:exec@stop-grafana \
         exec:exec@stop-container \
-        exec:exec@destroy-pod |& tee -a $logfile
+        exec:exec@destroy-pod |& tee -a "${logfile}"
     if [ $? -ne 0 ]; then
         failures=$((failures+1))
     fi
@@ -38,5 +38,5 @@ done
 echo
 echo "########################"
 echo "Test runs completed"
-echo "Failures: $failures"
-echo "Runs: $runs"
+echo "Failures: ${failures}"
+echo "Runs: ${runs}"
