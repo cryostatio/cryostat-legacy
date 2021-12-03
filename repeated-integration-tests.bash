3bdd4f73670ed5ee77c879cee527bbeb60877b3c
Andrew Azores <aazores@redhat.com>
2021-12-03 21:01:23 +0000

    fix(itest): correct check of test run command exit status (#769)
    * style(itest): quote variables
    
    * chore(itest): default to 1 run if not specified
    
    * fix(itest): capture test client and cryostat server logs
    
    * fix(itest): correct check of test run command exit status
    
    * ignore logs
    
    * style(itest): optionally retain test script stdout colouring
    
    * rename file
    
    * don't print command -v output
    
    * quote missed variable
    
    * use pipefail
    
    * update repeated-unit-tests to match repeated-integration-tests
    
    * destroy pod after retrieving logs
    
    * quote last command exit status
    
    * add failsafe:verify execution to ensure failed itests cause non-0 exit
    
    * use failure count as exit status
    
    * default to 1 run if not specified
    
    * fixup! use failure count as exit status
    
    * don't force exit status 1
    
    * ensure testing pod is torn down on exit
    
    * report actual vs request run count
    
    * remove redundant cleanup
    
    * fix broken argument regex check
    
    * split start/stop flags and ensure container logs are read before stopping
    
    * write test logs into target/
    
    * more tinkering with saving container logs correctly
    
    * clean up pod on enter or exit script to ensure clean slate
    
    * append log files, not overwrite

---
+++ repeated-integration-tests.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -o pipefail
+
+failures=0
+numeric='^[0-9]+$'
+if [[ "$1" =~ $numeric ]]; then
+    runs="$1"
+else
+    runs=1
+fi
+
+POD_NAME=cryostat-itests
+
+function cleanup() {
+    if podman pod exists "${POD_NAME}"; then
+        mvn exec:exec@destroy-pod
+    fi
+}
+trap cleanup EXIT
+cleanup
+
+STARTFLAGS=(
+    "exec:exec@create-pod"
+    "exec:exec@start-jfr-datasource"
+    "exec:exec@start-grafana"
+    "exec:exec@start-container"
+    "exec:exec@wait-for-container"
+    "failsafe:integration-test"
+    "failsafe:verify"
+)
+
+STOPFLAGS=(
+    "exec:exec@stop-jfr-datasource"
+    "exec:exec@stop-grafana"
+    "exec:exec@stop-container"
+    "exec:exec@destroy-pod"
+)
+
+if command -v ansi2txt >/dev/null; then
+    STARTFLAGS+=("-Dstyle.color=always")
+    STOPFLAGS+=("-Dstyle.color=always")
+    PIPECLEANER=ansi2txt
+else
+    PIPECLEANER=cat
+fi
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+runcount=0
+while [ "${runcount}" -lt "${runs}" ]; do
+    timestamp="$(date -Iminutes)"
+    client_logfile="$DIR/target/${POD_NAME}-${timestamp}.client.log"
+    server_logfile="$DIR/target/${POD_NAME}-${timestamp}.server.log"
+    mvn "${STARTFLAGS[@]}" |& tee -a >($PIPECLEANER >> "${client_logfile}")
+    if [ "$?" -ne 0 ]; then
+        failures=$((failures+1))
+    fi
+    runcount=$((runcount+1))
+    podman pod logs -c cryostat-itest "${POD_NAME}" &>> "${server_logfile}"
+    mvn "${STOPFLAGS[@]}" |& tee -a >($PIPECLEANER >> "${client_logfile}")
+done
+
+echo
+echo "########################"
+echo "Test runs completed"
+echo "Failures: ${failures}"
+echo "Runs: ${runcount}/${runs}"
+
+exit ${failures}
---
+++ repeated-unit-tests.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o pipefail
+
+failures=0
+numeric='^[0-9]+$'
+if [[ "$1" =~ $numeric ]]; then
+    runs="$1"
+else
+    runs=1
+fi
+
+FLAGS=(
+    "-Dcryostat.minimal=true"
+)
+
+if command -v ansi2txt >/dev/null; then
+    FLAGS+=("-Dstyle.color=always")
+    PIPECLEANER=ansi2txt
+else
+    PIPECLEANER=cat
+fi
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+runcount=0
+while [ "${runcount}" -lt ${runs} ]; do
+    logfile="$DIR/target/cryostat-unittests-$(date -Iminutes).log"
+    mvn "${FLAGS[@]}" surefire:test |& tee >($PIPECLEANER > "${logfile}")
+    if [ "$?" -ne 0 ]; then
+        failures=$((failures+1))
+    fi
+    runcount=$((runcount+1))
+done
+
+echo
+echo "########################"
+echo "Test runs completed"
+echo "Failures: ${failures}"
+echo "Runs: ${runcount}/${runs}"
+
+exit ${failures}
