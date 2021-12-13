b4945aed306df53da4b2937308ecb2645e6d75a5
Andrew Azores <aazores@redhat.com>
2021-12-13 10:32:51 -0500

    feat(graphql): custom targets querying

--- GRAPHQL.md
+++ GRAPHQL.md
@@ -12,7 +12,8 @@ Accepts `GET` or `POST` requests to perform GraphQL queries. See:
 for some more info.
 
 src/main/java/io/cryostat/net/web/http/api/beta/graph/GraphModule.java
-contains the mock data that can be queried.
+contains the mock data that can be queried as well as bindings for the GraphQL
+engine to query custom targets.
 
 ```bash
 $ https :8181/api/beta/graphql query=@sample-query.graphql
@@ -30,6 +31,112 @@ content-type: application/json
             },
             "pageCount": 223
         }
+    }
+}
+```
+
+```bash
+$ https -f :8181/api/v2/targets alias=foo connectUrl=localhost:0 annotations.cryostat.HOST=localhost
+HTTP/1.1 200 OK
+content-encoding: gzip
+content-length: 156
+content-type: application/json
+
+{
+    "data": {
+        "result": {
+            "alias": "foo",
+            "annotations": {
+                "cryostat": {
+                    "HOST": "localhost"
+                },
+                "platform": {}
+            },
+            "connectUrl": "localhost:0",
+            "labels": {}
+        }
+    },
+    "meta": {
+        "status": "OK",
+        "type": "application/json"
+    }
+}
+
+
+$ https -v :8181/api/beta/graphql query=@targets-query.graphql
+POST /api/beta/graphql HTTP/1.1
+Accept: application/json, */*;q=0.5
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Content-Length: 140
+Content-Type: application/json
+Host: localhost:8181
+User-Agent: HTTPie/2.6.0
+
+{
+    "query": "{\n  customTargets {\n    serviceUri\n    alias\n    labels\n    annotations {\n      cryostat\n      platform\n    }\n  }\n}\n"
+}
+
+
+HTTP/1.1 200 OK
+content-encoding: gzip
+content-length: 138
+content-type: application/json
+
+{
+    "data": {
+        "customTargets": [
+            {
+                "alias": "foo",
+                "annotations": {
+                    "cryostat": {
+                        "HOST": "localhost"
+                    },
+                    "platform": {}
+                },
+                "labels": {},
+                "serviceUri": "localhost:0"
+            }
+        ]
+    }
+}
+
+
+$ https -v :8181/api/beta/graphql query=@targets-by-annotation-query.graphql
+POST /api/beta/graphql HTTP/1.1
+Accept: application/json, */*;q=0.5
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Content-Length: 178
+Content-Type: application/json
+Host: localhost:8181
+User-Agent: HTTPie/2.6.0
+
+{
+    "query": "{\n  customTargetsWithAnnotation(annotation: \"HOST\") {\n    serviceUri\n    alias\n    labels\n    annotations {\n      cryostat\n      platform\n    }\n  }\n}\n\n"
+}
+
+
+HTTP/1.1 200 OK
+content-encoding: gzip
+content-length: 144
+content-type: application/json
+
+{
+    "data": {
+        "customTargetsWithAnnotation": [
+            {
+                "alias": "foo",
+                "annotations": {
+                    "cryostat": {
+                        "HOST": "localhost"
+                    },
+                    "platform": {}
+                },
+                "labels": {},
+                "serviceUri": "localhost:0"
+            }
+        ]
     }
 }
 ```
--- src/main/java/io/cryostat/net/web/http/api/beta/graph/GraphModule.java
+++ src/main/java/io/cryostat/net/web/http/api/beta/graph/GraphModule.java
@@ -39,21 +39,25 @@ package io.cryostat.net.web.http.api.beta.graph;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import io.cryostat.net.web.http.RequestHandler;
+import io.cryostat.platform.ServiceRef;
+import io.cryostat.platform.internal.CustomTargetPlatformClient;
 
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.IntoSet;
 import graphql.GraphQL;
+import graphql.scalars.ExtendedScalars;
 import graphql.schema.DataFetcher;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -114,26 +118,71 @@ public abstract class GraphModule {
     @Provides
     @Singleton
     static GraphQL provideGraphQL(
+            @Named("customTargets") DataFetcher<List<ServiceRef>> customTargetsFetcher,
+            @Named("customTargetsWithAnnotation")
+                    DataFetcher<List<ServiceRef>> customTargetsWithAnnotationFetcher,
             @Named("bookById") DataFetcher<Map<String, String>> bookByIdFetcher,
             @Named("author") DataFetcher<Map<String, String>> authorFetcher) {
+        RuntimeWiring wiring =
+                RuntimeWiring.newRuntimeWiring()
+                        .scalar(ExtendedScalars.Object)
+                        .type(
+                                TypeRuntimeWiring.newTypeWiring("Query")
+                                        .dataFetcher("customTargets", customTargetsFetcher))
+                        .type(
+                                TypeRuntimeWiring.newTypeWiring("Query")
+                                        .dataFetcher(
+                                                "customTargetsWithAnnotation",
+                                                customTargetsWithAnnotationFetcher))
+                        .type(
+                                TypeRuntimeWiring.newTypeWiring("Query")
+                                        .dataFetcher("bookById", bookByIdFetcher))
+                        .type(
+                                TypeRuntimeWiring.newTypeWiring("Book")
+                                        .dataFetcher("author", authorFetcher))
+                        .build();
         try (InputStream is = GraphModule.class.getResourceAsStream("/schema.graphql")) {
-            String sdl = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            TypeDefinitionRegistry tdr = new SchemaParser().parse(sdl);
-            RuntimeWiring wiring =
-                    RuntimeWiring.newRuntimeWiring()
-                            .type(
-                                    TypeRuntimeWiring.newTypeWiring("Query")
-                                            .dataFetcher("bookById", bookByIdFetcher))
-                            .type(
-                                    TypeRuntimeWiring.newTypeWiring("Book")
-                                            .dataFetcher("author", authorFetcher))
-                            .build();
-
+            TypeDefinitionRegistry tdr = new SchemaParser().parse(is);
             return GraphQL.newGraphQL(new SchemaGenerator().makeExecutableSchema(tdr, wiring))
                     .build();
         } catch (IOException ioe) {
             throw new RuntimeException(ioe);
         }
+    }
+
+    @Provides
+    @Singleton
+    @Named("customTargets")
+    static DataFetcher<List<ServiceRef>> provideCustomTargetsFetcher(
+            CustomTargetPlatformClient client) {
+        return env -> client.listDiscoverableServices();
+    }
+
+    @Provides
+    @Singleton
+    @Named("customTargetsWithAnnotation")
+    static DataFetcher<List<ServiceRef>> provideCustomTargetsWithAnnotationFetcher(
+            CustomTargetPlatformClient client) {
+        return env -> {
+            String annotationKey = env.getArgument("annotation");
+            List<ServiceRef> withPlatformAnnotation =
+                    client.listDiscoverableServices().stream()
+                            .filter(c -> c.getPlatformAnnotations().containsKey(annotationKey))
+                            .collect(Collectors.toList());
+            List<ServiceRef> withCryostatAnnotation =
+                    client.listDiscoverableServices().stream()
+                            .filter(
+                                    c ->
+                                            c.getCryostatAnnotations().keySet().stream()
+                                                    .map(Enum::name)
+                                                    .anyMatch(k -> k.equals(annotationKey)))
+                            .collect(Collectors.toList());
+
+            List<ServiceRef> result = new ArrayList<>();
+            result.addAll(withPlatformAnnotation);
+            result.addAll(withCryostatAnnotation);
+            return result;
+        };
     }
 
     @Provides
