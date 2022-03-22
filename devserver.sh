e7409bc18b78475e68cd1832ddf015c29c423485
Andrew Azores <aazores@redhat.com>
2022-03-22 17:22:35 -0400

    note credentials in startup script

--- devserver.sh
+++ devserver.sh
@@ -33,6 +33,7 @@ if [ -z "$CRYOSTAT_AUTH_MANAGER" ]; then
     CRYOSTAT_AUTH_MANAGER="io.cryostat.net.BasicAuthManager"
 fi
 if  [ "$CRYOSTAT_AUTH_MANAGER" = "io.cryostat.net.BasicAuthManager" ]; then
+    # credentials `user:pass`
     echo "user:d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1" > "${work_dir}/conf/cryostat-users.properties"
 fi
 
