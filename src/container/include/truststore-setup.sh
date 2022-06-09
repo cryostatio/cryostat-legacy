102c9a048146be63d168e009153d4a688d3196dd
Andrew Azores <aazores@redhat.com>
2022-06-09 15:49:40 -0400

    update truststore setup for JDK17

--- src/container/include/truststore-setup.sh
+++ src/container/include/truststore-setup.sh
@@ -16,7 +16,7 @@ pushd $CONF_DIR
 keytool -importkeystore \
     -noprompt \
     -storetype PKCS12 \
-    -srckeystore /usr/lib/jvm/jre-11-openjdk/lib/security/cacerts \
+    -srckeystore /usr/lib/jvm/jre-17-openjdk/lib/security/cacerts \
     -srcstorepass changeit \
     -destkeystore "$SSL_TRUSTSTORE" \
     -deststorepass "$SSL_TRUSTSTORE_PASS"
