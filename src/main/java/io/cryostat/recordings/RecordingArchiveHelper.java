85d80f4d2eb5cf834d2127a4bd01ad52f6d8ebb3
Hareet Dhillon <hdhillon@redhat.com>
2022-08-17 18:34:37 -0400

    fixup! fixup! Add target-specific RecordingDeleteHandler

--- src/main/java/io/cryostat/recordings/RecordingArchiveHelper.java
+++ src/main/java/io/cryostat/recordings/RecordingArchiveHelper.java
@@ -295,7 +295,7 @@ public class RecordingArchiveHelper {
         String subdirectory;
         if (sourceTarget == null) {
             subdirectory = "default";
-        } else if (sourceTarget.equals("")) {
+        } else if (sourceTarget.equals(UPLOADED_RECORDINGS_SUBDIRECTORY)) {
             subdirectory = UPLOADED_RECORDINGS_SUBDIRECTORY;
         } else {
             subdirectory = base32.encodeAsString(sourceTarget.getBytes(StandardCharsets.UTF_8));
@@ -451,7 +451,7 @@ public class RecordingArchiveHelper {
         CompletableFuture<Path> future = new CompletableFuture<>();
         try {
             String subdirectoryName =
-                    (sourceTarget.equals(""))
+                    (sourceTarget.equals(UPLOADED_RECORDINGS_SUBDIRECTORY))
                             ? UPLOADED_RECORDINGS_SUBDIRECTORY
                             : base32.encodeAsString(sourceTarget.getBytes(StandardCharsets.UTF_8));
             Path subdirectory = archivedRecordingsPath.resolve(subdirectoryName);
