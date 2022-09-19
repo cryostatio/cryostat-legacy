7076cca245c0651f0157c140e4dbcb8ff7d3b170
Andrew Azores <aazores@redhat.com>
2022-09-19 18:13:07 +0000

    test(itest): use standard external image version, make JDP poll configurable (#1069)
    * all itests use vertx-fib-demo:0.8.0
    
    extract constant
    
    * remove unnecessary credentials definitions
    
    * use liveness probe endpoint for startup check
    
    * make JDP poll configurable by properties

--- src/test/java/itest/DiscoveryIT.java
+++ src/test/java/itest/DiscoveryIT.java
@@ -72,8 +72,7 @@ class DiscoveryIT extends ExternalTargetsTest {
         for (int i = 0; i < NUM_EXT_CONTAINERS; i++) {
             specs.add(
                     new Podman.ImageSpec(
-                            "quay.io/andrewazores/vertx-fib-demo:0.7.0",
-                            Map.of("JMX_PORT", String.valueOf(9093 + i))));
+                            FIB_DEMO_IMAGESPEC, Map.of("JMX_PORT", String.valueOf(9093 + i))));
         }
         for (Podman.ImageSpec spec : specs) {
             CONTAINERS.add(Podman.run(spec));
