bc94947b6897ba837b7cfc9c6d07573cd0ef996a
Max Cao <macao@redhat.com>
2023-02-02 14:56:51 -0500

    repeated-integration-tests

--- repeated-integration-tests.bash
+++ repeated-integration-tests.bash
@@ -15,16 +15,24 @@ if [ -z "${MVN}" ]; then
     MVN="$(which mvn)"
 fi
 
+getPomProperty() {
+    if command -v xpath > /dev/null 2>&1 ; then
+        xpath -q -e "project/properties/$1/text()" pom.xml
+    else
+        ${MVN} help:evaluate -o -B -q -DforceStdout -Dexpression="$1"
+    fi
+}
+
 if [ -z "${POD_NAME}" ]; then
-    POD_NAME="$(${MVN} help:evaluate -o -B -q -DforceStdout -Dexpression=cryostat.itest.podName)"
+    POD_NAME="$(getPomProperty cryostat.itest.podName)"
 fi
 
 if [ -z "${CONTAINER_NAME}" ]; then
-    CONTAINER_NAME="$(${MVN} help:evaluate -o -B -q -DforceStdout -Dexpression=cryostat.itest.containerName)"
+    CONTAINER_NAME="$(getPomProperty cryostat.itest.containerName)"
 fi
 
 if [ -z "${ITEST_IMG_VERSION}" ]; then
-    ITEST_IMG_VERSION="$(${MVN} help:evaluate -o -B -q -DforceStdout -Dexpression=project.version)"
+    ITEST_IMG_VERSION="$(getPomProperty project.version)"
     ITEST_IMG_VERSION="${ITEST_IMG_VERSION,,}" # lowercase
 fi
 
