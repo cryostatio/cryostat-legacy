f4aef38be60ab46d9de197d8c1bf8a17882a8676
Andrew Azores <aazores@redhat.com>
2023-04-14 16:59:18 -0400

    Revert "suppress warning"
    This reverts commit 2b86d5b7ebcf57fbd1f90310e5d02ccad82ed947.

--- src/main/java/io/cryostat/discovery/BuiltInDiscovery.java
+++ src/main/java/io/cryostat/discovery/BuiltInDiscovery.java
@@ -56,7 +56,6 @@ import io.cryostat.platform.internal.CustomTargetPlatformClient;
 import io.cryostat.platform.internal.PlatformDetectionStrategy;
 
 import dagger.Lazy;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
@@ -90,9 +89,6 @@ public class BuiltInDiscovery extends AbstractVerticle implements Consumer<Targe
         this.logger = logger;
     }
 
-    @SuppressFBWarnings(
-            value = "RV_RETURN_VALUE_IGNORED_BAD_PRACTICE",
-            justification = "executor.submit() return value is ignored, the Future is never needed")
     @Override
     public void start(Promise<Void> start) {
         storage.addTargetDiscoveryListener(this);
