ef38a62b5d51a2eeaabd338c7d9a2477473e5a1b
Atif Ali <atali@redhat.com>
2023-06-30 10:27:30 -0400

    resolved conversation

--- src/main/java/io/cryostat/net/SslConfiguration.java
+++ src/main/java/io/cryostat/net/SslConfiguration.java
@@ -65,7 +65,7 @@ public class SslConfiguration {
 
         if (env.hasEnv(Variables.DISABLE_SSL)) {
             String disableSslValue = env.getEnv(Variables.DISABLE_SSL);
-            if ("true".equalsIgnoreCase(disableSslValue) || "TRUE".equalsIgnoreCase(disableSslValue)) {
+            if ("true".equalsIgnoreCase(disableSslValue.toLowerCase())) {
                 strategy = new NoSslStrategy();
                 logger.info("Selected NoSSL strategy");
                 return;
