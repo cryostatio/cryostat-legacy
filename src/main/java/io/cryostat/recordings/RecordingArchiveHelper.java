cryostatio/cryostat-legacy

commit 70cf51f0cd1bb19c41b98e80c03b947cab49092a

chore(recordings): replace subdirectory with jvmId in fromPathHandler (#1692)

src/main/java/io/cryostat/recordings/RecordingArchiveHelper.java
@@ -709,8 +709,7 @@ public class RecordingArchiveHelper {
                                                                         "beta/recordings",
                                                                         "beta/fs/recordings"),
                                                         webServer
-                                                                .getArchivedReportURL(
-                                                                        subdirectoryName, file)
+                                                                .getArchivedReportURL(jvmId, file)
                                                                 .replace(
                                                                         "beta/reports",
                                                                         "beta/fs/reports"),

src/main/java/io/cryostat/net/web/http/api/beta/RecordingDeleteFromPathHandler.java
@@ -31,6 +31,7 @@ import io.cryostat.net.web.http.api.v2.AbstractV2RequestHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
 import io.cryostat.net.web.http.api.v2.IntermediateResponse;
 import io.cryostat.net.web.http.api.v2.RequestParameters;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivePathException;
@@ -40,17 +41,20 @@ import io.vertx.core.http.HttpMethod;
 
 public class RecordingDeleteFromPathHandler extends AbstractV2RequestHandler<Void> {
 
-    static final String PATH = "fs/recordings/:subdirectoryName/:recordingName";
+    static final String PATH = "fs/recordings/:jvmId/:recordingName";
 
     private final RecordingArchiveHelper recordingArchiveHelper;
+    private final JvmIdHelper jvmIdHelper;
 
     @Inject
     RecordingDeleteFromPathHandler(
             AuthManager auth,
             CredentialsManager credentialsManager,
             Gson gson,
+            JvmIdHelper jvmIdHelper,
             RecordingArchiveHelper recordingArchiveHelper) {
         super(auth, credentialsManager, gson);
+        this.jvmIdHelper = jvmIdHelper;
         this.recordingArchiveHelper = recordingArchiveHelper;
     }
 
@@ -91,9 +95,10 @@ public class RecordingDeleteFromPathHandler extends AbstractV2RequestHandler<Voi
 
     @Override
     public IntermediateResponse<Void> handle(RequestParameters params) throws Exception {
-        String subdirectoryName = params.getPathParams().get("subdirectoryName");
         String recordingName = params.getPathParams().get("recordingName");
+        String jvmId = params.getPathParams().get("jvmId");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             recordingArchiveHelper.deleteRecordingFromPath(subdirectoryName, recordingName).get();
             return new IntermediateResponse<Void>().body(null);
         } catch (ExecutionException e) {

src/main/java/io/cryostat/net/web/http/api/beta/RecordingGetFromPathWithJwtHandler.java
@@ -32,6 +32,7 @@ import io.cryostat.net.web.http.HttpMimeType;
 import io.cryostat.net.web.http.api.ApiVersion;
 import io.cryostat.net.web.http.api.v2.AbstractAssetJwtConsumingHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivePathException;
@@ -44,8 +45,9 @@ import io.vertx.ext.web.RoutingContext;
 
 public class RecordingGetFromPathWithJwtHandler extends AbstractAssetJwtConsumingHandler {
 
-    static final String PATH = "fs/recordings/:subdirectoryName/:recordingName/jwt";
+    static final String PATH = "fs/recordings/:jvmId/:recordingName/jwt";
 
+    private final JvmIdHelper jvmIdHelper;
     private final RecordingArchiveHelper recordingArchiveHelper;
 
     @Inject
@@ -54,9 +56,11 @@ public class RecordingGetFromPathWithJwtHandler extends AbstractAssetJwtConsumin
             CredentialsManager credentialsManager,
             AssetJwtHelper jwtFactory,
             Lazy<WebServer> webServer,
+            JvmIdHelper jvmIdHelper,
             RecordingArchiveHelper recordingArchiveHelper,
             Logger logger) {
         super(auth, credentialsManager, jwtFactory, webServer, logger);
+        this.jvmIdHelper = jvmIdHelper;
         this.recordingArchiveHelper = recordingArchiveHelper;
     }
 
@@ -87,9 +91,10 @@ public class RecordingGetFromPathWithJwtHandler extends AbstractAssetJwtConsumin
 
     @Override
     public void handleWithValidJwt(RoutingContext ctx, JWT jwt) throws Exception {
-        String subdirectoryName = ctx.pathParam("subdirectoryName");
+        String jvmId = ctx.pathParam("jvmId");
         String recordingName = ctx.pathParam("recordingName");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             Path archivedRecording =
                     recordingArchiveHelper
                             .getRecordingPathFromPath(subdirectoryName, recordingName)

src/main/java/io/cryostat/net/web/http/api/beta/RecordingMetadataLabelsPostFromPathHandler.java
@@ -30,6 +30,7 @@ import io.cryostat.net.web.http.api.v2.AbstractV2RequestHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
 import io.cryostat.net.web.http.api.v2.IntermediateResponse;
 import io.cryostat.net.web.http.api.v2.RequestParameters;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingMetadataManager;
 import io.cryostat.recordings.RecordingMetadataManager.Metadata;
@@ -41,21 +42,24 @@ import io.vertx.core.http.HttpMethod;
 
 public class RecordingMetadataLabelsPostFromPathHandler extends AbstractV2RequestHandler<Metadata> {
 
-    static final String PATH = "fs/recordings/:subdirectoryName/:recordingName/metadata/labels";
+    static final String PATH = "fs/recordings/:jvmId/:recordingName/metadata/labels";
 
     private final RecordingArchiveHelper recordingArchiveHelper;
     private final RecordingMetadataManager recordingMetadataManager;
+    private final JvmIdHelper jvmIdHelper;
 
     @Inject
     RecordingMetadataLabelsPostFromPathHandler(
             AuthManager auth,
             CredentialsManager credentialsManager,
             Gson gson,
+            JvmIdHelper jvmIdHelper,
             RecordingArchiveHelper recordingArchiveHelper,
             RecordingMetadataManager recordingMetadataManager) {
         super(auth, credentialsManager, gson);
         this.recordingArchiveHelper = recordingArchiveHelper;
         this.recordingMetadataManager = recordingMetadataManager;
+        this.jvmIdHelper = jvmIdHelper;
     }
 
     @Override
@@ -96,9 +100,9 @@ public class RecordingMetadataLabelsPostFromPathHandler extends AbstractV2Reques
     @Override
     public IntermediateResponse<Metadata> handle(RequestParameters params) throws Exception {
         String recordingName = params.getPathParams().get("recordingName");
-        String subdirectoryName = params.getPathParams().get("subdirectoryName");
-
+        String jvmId = params.getPathParams().get("jvmId");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             Metadata metadata =
                     new Metadata(recordingMetadataManager.parseRecordingLabels(params.getBody()));
 

src/main/java/io/cryostat/net/web/http/api/beta/RecordingUploadPostFromPathHandler.java
@@ -41,6 +41,7 @@ import io.cryostat.net.web.http.api.v2.AbstractV2RequestHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
 import io.cryostat.net.web.http.api.v2.IntermediateResponse;
 import io.cryostat.net.web.http.api.v2.RequestParameters;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivePathException;
@@ -56,11 +57,12 @@ import org.apache.commons.validator.routines.UrlValidator;
 
 class RecordingUploadPostFromPathHandler extends AbstractV2RequestHandler<String> {
 
-    static final String PATH = "fs/recordings/:subdirectoryName/:recordingName/upload";
+    static final String PATH = "fs/recordings/:jvmId/:recordingName/upload";
 
     private final Environment env;
     private final long httpTimeoutSeconds;
     private final WebClient webClient;
+    private final JvmIdHelper jvmIdHelper;
     private final RecordingArchiveHelper recordingArchiveHelper;
 
     @Inject
@@ -70,12 +72,14 @@ class RecordingUploadPostFromPathHandler extends AbstractV2RequestHandler<String
             Environment env,
             @Named(HttpModule.HTTP_REQUEST_TIMEOUT_SECONDS) long httpTimeoutSeconds,
             WebClient webClient,
+            JvmIdHelper jvmIdHelper,
             RecordingArchiveHelper recordingArchiveHelper,
             Gson gson) {
         super(auth, credentialsManager, gson);
         this.env = env;
         this.httpTimeoutSeconds = httpTimeoutSeconds;
         this.webClient = webClient;
+        this.jvmIdHelper = jvmIdHelper;
         this.recordingArchiveHelper = recordingArchiveHelper;
     }
 
@@ -116,9 +120,10 @@ class RecordingUploadPostFromPathHandler extends AbstractV2RequestHandler<String
 
     @Override
     public IntermediateResponse<String> handle(RequestParameters params) throws Exception {
-        String subdirectoryName = params.getPathParams().get("subdirectoryName");
         String recordingName = params.getPathParams().get("recordingName");
+        String jvmId = params.getPathParams().get("jvmId");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             URL uploadUrl = new URL(env.getEnv(Variables.GRAFANA_DATASOURCE_ENV));
             boolean isValidUploadUrl =
                     new UrlValidator(UrlValidator.ALLOW_LOCAL_URLS).isValid(uploadUrl.toString());

src/main/java/io/cryostat/net/web/http/api/beta/ReportGetFromPathHandler.java
@@ -38,6 +38,7 @@ import io.cryostat.net.web.http.api.v2.AbstractV2RequestHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
 import io.cryostat.net.web.http.api.v2.IntermediateResponse;
 import io.cryostat.net.web.http.api.v2.RequestParameters;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivePathException;
 
@@ -47,8 +48,9 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 
 public class ReportGetFromPathHandler extends AbstractV2RequestHandler<Path> {
 
-    static final String PATH = "fs/reports/:subdirectoryName/:recordingName";
+    static final String PATH = "fs/reports/:jvmId/:recordingName";
 
+    private final JvmIdHelper jvmIdHelper;
     private final ReportService reportService;
     private final long reportGenerationTimeoutSeconds;
 
@@ -57,10 +59,12 @@ public class ReportGetFromPathHandler extends AbstractV2RequestHandler<Path> {
             AuthManager auth,
             CredentialsManager credentialsManager,
             Gson gson,
+            JvmIdHelper jvmIdHelper,
             ReportService reportService,
             @Named(ReportsModule.REPORT_GENERATION_TIMEOUT_SECONDS)
                     long reportGenerationTimeoutSeconds) {
         super(auth, credentialsManager, gson);
+        this.jvmIdHelper = jvmIdHelper;
         this.reportService = reportService;
         this.reportGenerationTimeoutSeconds = reportGenerationTimeoutSeconds;
     }
@@ -105,9 +109,10 @@ public class ReportGetFromPathHandler extends AbstractV2RequestHandler<Path> {
 
     @Override
     public IntermediateResponse<Path> handle(RequestParameters params) throws Exception {
-        String subdirectoryName = params.getPathParams().get("subdirectoryName");
+        String jvmId = params.getPathParams().get("jvmId");
         String recordingName = params.getPathParams().get("recordingName");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             List<String> queriedFilter = params.getQueryParams().getAll("filter");
             String rawFilter = queriedFilter.isEmpty() ? "" : queriedFilter.get(0);
             Path report =

src/main/java/io/cryostat/net/web/http/api/beta/ReportGetFromPathWithJwtHandler.java
@@ -38,6 +38,7 @@ import io.cryostat.net.web.http.HttpMimeType;
 import io.cryostat.net.web.http.api.ApiVersion;
 import io.cryostat.net.web.http.api.v2.AbstractAssetJwtConsumingHandler;
 import io.cryostat.net.web.http.api.v2.ApiException;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivePathException;
@@ -51,10 +52,11 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 
 class ReportGetFromPathWithJwtHandler extends AbstractAssetJwtConsumingHandler {
 
-    static final String PATH = "fs/reports/:subdirectoryName/:recordingName/jwt";
+    static final String PATH = "fs/reports/:jvmId/:recordingName/jwt";
 
     private final ReportService reportService;
     private final long generationTimeoutSeconds;
+    private final JvmIdHelper jvmIdHelper;
 
     @Inject
     ReportGetFromPathWithJwtHandler(
@@ -62,11 +64,13 @@ class ReportGetFromPathWithJwtHandler extends AbstractAssetJwtConsumingHandler {
             CredentialsManager credentialsManager,
             AssetJwtHelper jwtFactory,
             Lazy<WebServer> webServer,
+            JvmIdHelper jvmIdHelper,
             ReportService reportService,
             RecordingArchiveHelper recordingArchiveHelper,
             @Named(ReportsModule.REPORT_GENERATION_TIMEOUT_SECONDS) long generationTimeoutSeconds,
             Logger logger) {
         super(auth, credentialsManager, jwtFactory, webServer, logger);
+        this.jvmIdHelper = jvmIdHelper;
         this.reportService = reportService;
         this.generationTimeoutSeconds = generationTimeoutSeconds;
     }
@@ -111,9 +115,10 @@ class ReportGetFromPathWithJwtHandler extends AbstractAssetJwtConsumingHandler {
 
     @Override
     public void handleWithValidJwt(RoutingContext ctx, JWT jwt) throws Exception {
-        String subdirectoryName = ctx.pathParam("subdirectoryName");
+        String jvmId = ctx.pathParam("jvmId");
         String recordingName = ctx.pathParam("recordingName");
         try {
+            String subdirectoryName = jvmIdHelper.jvmIdToSubdirectoryName(jvmId);
             List<String> queriedFilter = ctx.queryParam("filter");
             String rawFilter = queriedFilter.isEmpty() ? "" : queriedFilter.get(0);
             Path report =

src/test/java/io/cryostat/net/web/http/api/beta/RecordingDeleteFromPathHandlerTest.java
@@ -32,6 +32,7 @@ import io.cryostat.net.web.http.api.ApiVersion;
 import io.cryostat.net.web.http.api.v2.ApiException;
 import io.cryostat.net.web.http.api.v2.IntermediateResponse;
 import io.cryostat.net.web.http.api.v2.RequestParameters;
+import io.cryostat.recordings.JvmIdHelper;
 import io.cryostat.recordings.RecordingArchiveHelper;
 import io.cryostat.recordings.RecordingNotFoundException;
 import io.cryostat.rules.ArchivedRecordingInfo;
@@ -56,13 +57,14 @@ class RecordingDeleteFromPathHandlerTest {
     @Mock AuthManager auth;
     @Mock CredentialsManager credentialsManager;
     @Mock Gson gson;
+    @Mock JvmIdHelper jvmIdHelper;
     @Mock RecordingArchiveHelper recordingArchiveHelper;
 
     @BeforeEach
     void setup() {
         this.handler =
                 new RecordingDeleteFromPathHandler(
-                        auth, credentialsManager, gson, recordingArchiveHelper);
+                        auth, credentialsManager, gson, jvmIdHelper, recordingArchiveHelper);
     }
 
     @Nested
@@ -94,7 +96,7 @@ class RecordingDeleteFromPathHandlerTest {
         void shouldHandleCorrectPath() {
             MatcherAssert.assertThat(
                     handler.path(),
-                    Matchers.equalTo("/api/beta/fs/recordings/:subdirectoryName/:recordingName"));
+                    Matchers.equalTo("/api/beta/fs/recordings/:jvmId/:recordingName"));
         }
 
         @Test
@@ -117,14 +119,12 @@ class RecordingDeleteFromPathHandlerTest {
         @Test
         void shouldThrow404IfNoMatchingRecordingFound() throws Exception {
             String recordingName = "someRecording";
+            String jvmId = "id";
             String subdirectoryName = "someSubdirectory";
+
+            when(jvmIdHelper.jvmIdToSubdirectoryName(jvmId)).thenReturn(subdirectoryName);
             when(params.getPathParams())
-                    .thenReturn(
-                            Map.of(
-                                    "subdirectoryName",
-                                    subdirectoryName,
-                                    "recordingName",
-                                    recordingName));
+                    .thenReturn(Map.of("jvmId", jvmId, "recordingName", recordingName));
 
             Future<ArchivedRecordingInfo> future =
                     CompletableFuture.failedFuture(
@@ -141,14 +141,12 @@ class RecordingDeleteFromPathHandlerTest {
         @Test
         void shouldHandleSuccessfulDELETERequest() throws Exception {
             String recordingName = "someRecording";
+            String jvmId = "id";
             String subdirectoryName = "someSubdirectory";
+
+            when(jvmIdHelper.jvmIdToSubdirectoryName(jvmId)).thenReturn(subdirectoryName);
             when(params.getPathParams())
-                    .thenReturn(
-                            Map.of(
-                                    "recordingName",
-                                    recordingName,
-                                    "subdirectoryName",
-                                    subdirectoryName));
+                    .thenReturn(Map.of("recordingName", recordingName, "jvmId", jvmId));
 
             CompletableFuture<ArchivedRecordingInfo> future = Mockito.mock(CompletableFuture.class);
             when(recordingArchiveHelper.deleteRecordingFromPath(

commit d80f6221afa3d5227d945da29776a29cd89956c1

fix(recordings): fix bug caused by #1692 (#1746)

src/main/java/io/cryostat/recordings/RecordingArchiveHelper.java
@@ -703,8 +703,7 @@ public class RecordingArchiveHelper {
                                                         targetId,
                                                         file,
                                                         webServer
-                                                                .getArchivedDownloadURL(
-                                                                        subdirectoryName, file)
+                                                                .getArchivedDownloadURL(jvmId, file)
                                                                 .replace(
                                                                         "beta/recordings",
                                                                         "beta/fs/recordings"),